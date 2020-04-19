1bd654d84b272732a5d6463ca34d46544763d72a
Tramsi <tramsi2014@gmail.com>
2020-04-19 10:46:04 +0200

    Configuring postcss to run purgecss on condition

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,16 @@
+const environ = process.env.NODE_ENV;
 module.exports = {
   plugins: [
     // ...
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./dist/index.html', './dist/typography.html'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
+    environ === 'production' || environ === 'ci'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: ['./dist/index.html', './dist/typography.html'],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+      : null
     // ...
   ]
 };
