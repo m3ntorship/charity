5ba2700fdece3ed611fe00ce6107d9b62f4fb81b
user <great.nile@gmail.com>
2020-04-24 23:47:37 +0200

    add component examples (class and hooks) as reference

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-// import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return 'hello';
+class App extends React.Component {
+  state = { tests: [] };
+  componentDidMount() {
+    axios('http://charity-cms.m3ntorship.net/tests').then(({ data: tests }) => {
+      this.setState({ tests });
+    });
+  }
+
+  render() {
+    return this.state.tests.map(({ name }) => <div key={name}>{name}</div>);
+  }
 }
 
-export default App;
+const AppHooks = () => {
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    axios('http://charity-cms.m3ntorship.net/tests').then(({ data: tests }) => {
+      setTests(tests);
+    });
+  }, []);
+
+  return tests.map(({ name }) => <div key={name}>{name}</div>);
+};
+
+export { AppHooks, App };
