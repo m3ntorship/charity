d548c918e603532db820e20a47082dfd4408c05d
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-04-25 16:33:23 +0200

    convert numbers section to react component

---
+++ src/components/Numbers/index.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import dotsImage from './img/dots.png';
+import circleImage from './img/circle.png';
+import peopleImage from './img/people-we-help.jpg';
+
+class Numbers extends Component {
+  state = {
+    numbers: [
+      { title: 'donations', number: '88k', id: 1 },
+      { title: 'campaigns', number: '66k', id: 2 },
+      { title: 'volunteers', number: '70+', id: 3 },
+      { title: 'funds raised', number: '66k', id: 4 }
+    ]
+  };
+
+  getData = () => {
+    // get data from api
+    fetch('').then(res => {
+      this.setState({
+        numbers: res.data
+      });
+    });
+  };
+
+  componentDidMount() {
+    //this.getData();
+  }
+
+  render() {
+    const { numbers } = this.state;
+
+    const numbersList = numbers.map(item => {
+      return (
+        <div
+          className="statistics-content__item justify-end flex flex-col w-1/4"
+          key={item.id}
+        >
+          <span className="statistics-content__item__value text-center tracking-wide text-c200 text-xl font-light font-body leading-loose">
+            {item.number}
+          </span>
+          <h3 className="statistics-content__item__name mt-3 tracking-wide capitalize font-light text-c100 text-md">
+            {item.title}
+          </h3>
+        </div>
+      );
+    });
+    return (
+      <section className="numbers z-0 relative bg-c800 pb-0">
+        <div className="container">
+          <div className="statistics-wrapper">
+            <div className="statistics-wrapper__image bg-cover bg-no-repeat"></div>
+            <div className="statistics-numbers">
+              <div className="statistics-numbers__speak relative">
+                <p className="statistics-numbers__speak__text p-4 font-normal absolute text-center capitalize text-lg font-body font-light text-c000 bg-c400">
+                  numbers speak
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="statistics-content bg-c000">
+          <div className="container">
+            <div className="statistics-content__list p-16 bg-c800 flex text-center">
+              {numbersList}
+            </div>
+          </div>
+        </div>
+        <div className="statistics-wrapper__dots-image absolute">
+          <img src={dotsImage} alt="" />
+        </div>
+        <div className="statistics-wrapper__circle-image absolute">
+          <img src={circleImage} alt="" />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Numbers;
