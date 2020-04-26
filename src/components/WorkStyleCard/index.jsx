42eec4600d8020813fb3094bb9d7c7d0a6a2e916
AbdoMagdy <abdoman46@hotmail.com>
2020-04-27 01:01:00 +0200

    added dumb card component to conatain how we work cards data

---
+++ src/components/WorkStyleCard/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class WorkStyleCard extends React.Component {
+  render() {
+    return (
+      <div className="text-center flex flex-col col-div items-center my-10 md:my-auto">
+        <div className="icon-div h-48 w-48 border-solid border-8 rounded-full flex justify-center p-2 items-center border-c200">
+          <div className="image-container rounded-full"></div>
+          <img className="w-5/12" src={this.props.img} alt={this.props.title} />
+        </div>
+        <h3 className="showcase-row__heading my-2">{this.props.title}</h3>
+        <p className="text-sm py-2 leading-relaxed">{this.props.description}</p>
+      </div>
+    );
+  }
+}
