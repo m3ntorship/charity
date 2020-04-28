af7253e7f1e32dfd784f39ef539413e30364def2
zahraa333 <zahraa.khalil2020@gmail.com>
2020-04-28 13:48:23 +0200

    fix run build deployment issue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { charityAPI } from './clients';
 import Logo from './components/Logo';
 import MainContact from './components/MainContact';
-import Activities from './components/Activities';
 import FeaturedBanner from './components/FeaturedBanner';
+import Activities from './components/Activities';
 
 class App extends React.Component {
   render() {
@@ -173,7 +173,7 @@ class App extends React.Component {
             </div>
           </div>
         </section>
-        
+
         <Activities />
 
         <FeaturedBanner />
@@ -564,8 +564,8 @@ class App extends React.Component {
                 <blockquote className="feedback__quote__text mt-8 mb-4 text-center">
                   <p className="mb-8 leading-normal font-hairline">
                     This is due to their excellent service, competitive pricing
-                    and customer support. It’s throughly refresing to get such a
-                    personal touch.
+                    and customer support. It’s throughly refresing to get such
+                    a personal touch.
                   </p>
                   <h4 className="text-c200 font-medium text-md font-semibold mb-2">
                     Christine Rose
--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { charityAPI } from '../../clients';
 import './styles.css';
 
-
 export default class Activities extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +9,8 @@ export default class Activities extends React.Component {
       activities_cards: [],
       title_primary: null,
       title_complementary: null,
-      description: null
+      description: null,
+      error: false
     };
     this.renderActivities = this.renderActivities.bind(this);
   }
@@ -18,14 +18,16 @@ export default class Activities extends React.Component {
   componentDidMount() {
     charityAPI({
       url: '/what-we-do'
-    }).then(({ data: activities }) => {
-      this.setState({
-        activities_cards: activities.how_we_work_cards,
-        description: activities.description,
-        title_primary: activities.title_primary,
-        title_complementary: activities.title_complementary
-      });
-    });
+    })
+      .then(({ data: activities }) => {
+        this.setState({
+          activities_cards: activities.how_we_work_cards,
+          description: activities.description,
+          title_primary: activities.title_primary,
+          title_complementary: activities.title_complementary
+        });
+      })
+      .catch(error => {});
   }
 
   renderActivities() {
@@ -49,7 +51,6 @@ export default class Activities extends React.Component {
   render() {
     return (
       <div className="activites container px-5 font-body text-c600">
-
         <ActivitiesHeader
           title_primary={this.state.title_primary}
           title_complementary={this.state.title_complementary}
@@ -60,7 +61,6 @@ export default class Activities extends React.Component {
         </div>
 
         <div className="heart-bg w-1/2 h-48 -mt-48 p-0"></div>
-
       </div>
     );
   }
@@ -81,71 +81,3 @@ class ActivitiesHeader extends React.Component {
     );
   }
 }
-
-// --------------------------------------------------------
-
-// <section className="activites container px-5 font-body text-c600">
-
-//   <div className="activities__intro flex flex-row">
-//     <h2 className="w-3/5 text-c100 font-bold leading-tighter">
-//       We Believe We Can Save More
-//       <span className="text-c200 font-hairline underline">Lives</span>
-//     </h2>
-//     <p className="w-2/5 mt-5">
-//       Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-//       commodo ligula eget dolor. Aenean massa.
-//     </p>
-//   </div>
-
-
-
-
-
-
-//   <div className="showcase-row -mt-3 px-8">
-//     <div className="text-center">
-//       <img
-//         className="mx-auto"
-//         src="assets/img/activities/01.png"
-//         alt=""
-//       />
-//       <h3 className="showcase-row__heading">We Help</h3>
-//       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-//     </div>
-
-
-
-//     <div className="activity ml-10 pl-10 relative text-center">
-//       <img
-//         className="mx-auto"
-//         src="assets/img/activities/02.png"
-//         alt=""
-//       />
-//       <h3 className="showcase-row__heading">We Build</h3>
-//       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-//     </div>
-
-//     <div className="activity ml-10 pl-10 relative text-center">
-//       <img
-//         className="mx-auto"
-//         src="assets/img/activities/03.png"
-//         alt=""
-//       />
-//       <h3 className="showcase-row__heading">We Educate</h3>
-//       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-//     </div>
-
-//     <div className="activity ml-10 pl-10 relative text-center">
-//       <img
-//         className="mx-auto"
-//         src="assets/img/activities/04.png"
-//         alt=""
-//       />
-//       <h3 className="showcase-row__heading">We Nourish</h3>
-//       <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-//     </div>
-//   </div>
-
-
-// </section>
-// <div className="heart-bg w-1/2 h-48 -mt-48 p-0"></div>
