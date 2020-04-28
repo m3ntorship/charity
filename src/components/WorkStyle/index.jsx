e5983250605a9946044f63370cad608490278f4e
AbdoMagdy <abdoman46@hotmail.com>
2020-04-28 22:28:51 +0200

    replaced calss with className

--- src/components/WorkStyle/index.jsx
+++ src/components/WorkStyle/index.jsx
@@ -21,16 +21,16 @@ export default class WorkStyle extends React.Component {
 
   render() {
     return (
-      <section class="work-style relative text-c600">
-        <div class="container">
-          <h2 class="text-center tracking-tight text-c100 font-bold">
+      <section className="work-style relative text-c600">
+        <div className="container">
+          <h2 className="text-center tracking-tight text-c100 font-bold">
             {this.state.Work.title_primary}{' '}
-            <span class="tracking-wide text-c200 font-hairline underline border-b-2">
+            <span className="tracking-wide text-c200 font-hairline underline border-b-2">
               {this.state.Work.title_complementary}
             </span>
           </h2>
 
-          <div class="work-style__items mx-auto showcase-row flex-col items-center md:flex-row md:items-start">
+          <div className="work-style__items mx-auto showcase-row flex-col items-center md:flex-row md:items-start">
             {(() => {
               if (this.state.Work.Cards) {
                 return this.state.Work.Cards.map(card => (
