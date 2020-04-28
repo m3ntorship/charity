b757d90f31380f4129e48d74b6305986ebb08bc1
malngaawy <mohager477@gmail.com>
2020-04-29 00:27:19 +0200

    make code organize and readable by ahmed ayoub

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -6,64 +6,78 @@ import './styles.css';
 export const ContactInfo = () => {
   const [contactData, setContactData] = useState(null);
   const [socialData, setSocialData] = useState(null);
+  const [fetchingDataError, setFetchingDataError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     Promise.all([
-      charityAPI('/main-contacts'),
-      charityAPI('/socialmedias')
-    ]).then(values => {
-      setContactData(values[0].data);
-      setSocialData(values[1].data);
-    });
+      charityAPI('/main-contacts').then(({ data }) => data),
+      charityAPI('/socialmedias').then(({ data }) => data)
+    ])
+      .then(([contacts, social]) => {
+        setContactData(contacts);
+        setSocialData(social);
+        setFetchingDataError(false);
+        setLoading(false);
+      })
+      .catch(error => {
+        setFetchingDataError(error);
+        setLoading(false);
+      });
   }, []);
 
-  // Dosen't mater witch one of status ... they come synchronicity
-  if (contactData) {
+  if (fetchingDataError) {
     return (
-      <div className="flex flex-wrap justify-between items-center mx-0 text-c000 w-full">
-        <div className="contact-section__social lg:w-1/4 flex justify-start text-xxs">
-          {socialData.map(({ _id, url, fontawesome_icons }) => (
-            <div
-              key={_id}
-              className="contact-section__social-icon hover:bg-c200 ml-0"
-            >
-              <a className="full-width-click" href={url} target="blank">
-                <i className={fontawesome_icons}></i>
-              </a>
-            </div>
-          ))}
-        </div>
-
-        <div className="contact-section__contacts lg:w-3/4 flex flex-wrap justify-end text-sm">
-          {contactData.map(
-            ({ _id, title, sub_title, icon: { url, name } }, index) => {
-              const isLast = index === contactData.length - 1;
-              return (
-                <div
-                  key={_id}
-                  className={cn('contact', {
-                    'border-right': !isLast
-                  })}
-                >
-                  <div className="contact-icon">
-                    <img className="h-auto" src={url} alt={name} />
-                  </div>
-                  <div className="information">
-                    <a className="block" href={url}>
-                      {title}
-                    </a>
-                    <small className="information-small leading-normal">
-                      {sub_title}
-                    </small>
-                  </div>
-                </div>
-              );
-            }
-          )}
-        </div>
-      </div>
+      <div>{`${fetchingDataError.message}: Error while fetching socialmedia or contacts`}</div>
     );
-  } else {
+  }
+
+  if (loading) {
     return <div>Loading .....</div>;
   }
+
+  return (
+    <div className="flex flex-wrap justify-between items-center mx-0 text-c000 w-full">
+      <div className="contact-section__social lg:w-1/4 flex justify-start text-xxs">
+        {socialData.map(({ _id, url, fontawesome_icons }) => (
+          <div
+            key={_id}
+            className="contact-section__social-icon hover:bg-c200 ml-0"
+          >
+            <a className="full-width-click" href={url} target="blank">
+              <i className={fontawesome_icons}></i>
+            </a>
+          </div>
+        ))}
+      </div>
+
+      <div className="contact-section__contacts lg:w-3/4 flex flex-wrap justify-end text-sm">
+        {contactData.map(
+          ({ _id, title, sub_title, icon: { url, name } }, index) => {
+            const isLast = index === contactData.length - 1;
+            return (
+              <div
+                key={_id}
+                className={cn('contact', {
+                  'border-right': !isLast
+                })}
+              >
+                <div className="contact-icon">
+                  <img className="h-auto" src={url} alt={name} />
+                </div>
+                <div className="information">
+                  <a className="block" href={url}>
+                    {title}
+                  </a>
+                  <small className="information-small leading-normal">
+                    {sub_title}
+                  </small>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
 };
