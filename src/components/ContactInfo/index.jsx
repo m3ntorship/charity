b743aaeab9edbd0064eba4b7c1c60f05e91049ee
malngaawy <mohager477@gmail.com>
2020-04-28 19:17:54 +0200

    fetch main contact after creating CT-main-contatcs

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -1,46 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { charityAPI } from '../../clients';
 import './styles.css';
-import location from './img/location.png';
-import mail from './img/mail.png';
-import phone from './img/phone.png';
-
-const contactInfoData = {
-  social_media_links: {
-    facebook: 'http//:www.facebook.com',
-    twitter: 'http//:www.twitter.com',
-    pinterest: 'http//:www.pinterest.com',
-    instagram: 'http//:www.instagram.com'
-  },
-
-  contact: {
-    email: {
-      email_address: 'needhelp@lovims.com',
-      icon: mail
-    },
-    phone: {
-      phone_number: '666 888 6666',
-      icon: phone
-    },
-    location: {
-      location_name: '66 Broklyn Street USA',
-      icon: location,
-      location_link:
-        'href="https://www.google.com/maps/place/66+Brooklyn+Ave,+Brooklyn,+NY+11216,+USA/@40.6817649,-73.9623869,13.75z/data=!4m5!3m4!1s0x89c25b84547857e3:0x2e1b27396ef429f!8m2!3d40.677763!4d-73.9444439"'
-    }
-  }
-};
-const charityContactAPI = () =>
-  Promise.resolve({
-    data: contactInfoData
-  });
 
 export const ContactInfo = () => {
   const [contactData, setContactData] = useState(null);
   const [socialData, setSocialData] = useState(null);
 
   useEffect(() => {
-    charityContactAPI('/contact-info')
+    charityAPI('/main-contacts')
       .then(({ data }) => {
         setContactData(data);
       })
@@ -63,20 +30,36 @@ export const ContactInfo = () => {
     return (
       <div className="flex flex-wrap justify-between items-center mx-0 text-c000">
         <div className="contact-section__social lg:w-1/4 flex justify-start text-xxs">
-          {socialData.map(item => (
+          {socialData.map(({ _id, url, fontawesome_icons }) => (
             <div
-              key={item.id}
+              key={_id}
               className="contact-section__social-icon hover:bg-c200 ml-0"
             >
-              <a className="full-width-click" href={item.url} target="blank">
-                <i className={item.fontawesome_icons}></i>
+              <a className="full-width-click" href={url} target="blank">
+                <i className={fontawesome_icons}></i>
               </a>
             </div>
           ))}
         </div>
 
         <div className="contact-section__contacts lg:w-3/4 flex flex-wrap text-sm">
-          <div className="contact border-right">
+          {contactData.map(({ _id, title, sub_title, icon: { url, name } }) => (
+            <div key={_id} className="contact border-right">
+              <div className="contact-icon">
+                <img className="h-auto" src={url} alt={name} />
+              </div>
+              <div className="information">
+                <a className="block" href={`mailto:${title}`}>
+                  {title}
+                </a>
+                <small className="information-small leading-normal">
+                  {sub_title}
+                </small>
+              </div>
+            </div>
+          ))}
+
+          {/* <div className="contact border-right">
             <div className="contact-icon">
               <img
                 className="h-auto"
@@ -136,7 +119,7 @@ export const ContactInfo = () => {
               </a>
               <small className="information-small">Visit us</small>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     );
