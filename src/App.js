5a9a74b91d24f98296fa5a8905e5e7cc83446b6c
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-04-30 01:05:14 +0200

    add News component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ContactTop from './components/ContactTop';
 import FeaturedBanner from './components/FeaturedBanner';
 import Activities from './components/Activities';
 import { ContactInfo } from './components/ContactInfo';
+import News from './components/NewsAndArticles';
 
 class App extends React.Component {
   render() {
@@ -586,71 +587,7 @@ class App extends React.Component {
           </div>
         </section>
 
-        <section className="news font-body bg-c800 mb-20 md:mb-48 pt-18 pb-1 md:pb-40 relative">
-          <div className="container">
-            <div className="head-section grid grid-cols-1 md:grid-cols-12">
-              <h2 className="text-c100 font-black md:col-span-5">
-                News &
-                <span className="text-c200 font-thin border-b-2">Articles</span>
-              </h2>
-              <p className="text-c600 text-lg md:mt-3 md:col-span-4 leading-relaxed">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-                quo eveniet recusandae molestiae reiciendis a sequi sapiente
-              </p>
-              <div className="btn-div md:col-span-3">
-                <button className="btn-sm text-sm bg-c300 my-8 md:float-right md:mt-3">
-                  View All Posts
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="container relative">
-            <div className="articles grid grid-cols-1 mt-12 md:mt-auto md:grid-cols-3 gap-8 md:absolute w-full">
-              <div className="article">
-                <img
-                  src="https://www.iapb.org/wp-content/uploads/30116455362_ed5a11aa91_o_0.jpg"
-                  alt="Africa Childres "
-                />
-                <div className="article-info transform -translate-y-1/2 bg-c000 py-6 text-center shadow-lg">
-                  <span className="text-c600 mx-2">
-                    <i className="fas fa-user-tie mr-1 text-c500"></i>Admin
-                  </span>
-                  <span className="text-c600 mx-2">
-                    <i className="fas fa-comments mr-1 text-c500"></i>2 Comments
-                  </span>
-                  <h4 className="text-c100 font-bold">Water Need in Africa</h4>
-                </div>
-              </div>
-
-              <div className="article">
-                <img
-                  src="https://globaladvocacyafrica.org/wp-content/uploads/2018/12/AFRICAN-CHILD-2-960x640.jpg"
-                  alt="Africa Childres "
-                />
-                <div className="article-info transform -translate-y-1/2 bg-c300 text-c100 text-center shadow-lg spicial-info cursor-pointer">
-                  <i className="fas fa-long-arrow-alt-right font-light"></i>
-                  Read More
-                </div>
-              </div>
-
-              <div className="article">
-                <img
-                  src="https://violenceagainstchildren.un.org/sites/violenceagainstchildren.un.org/files/styles/panopoly_image_full/public/news_articles/africaaidsfree.jpg?itok=o18cimXE"
-                  alt="Africa Childres "
-                />
-                <div className="article-info transform -translate-y-1/2 bg-c000 py-6 text-center shadow-lg">
-                  <span className="text-c600 mx-2">
-                    <i className="fas fa-user-tie mr-1 text-c500"></i>Admin
-                  </span>
-                  <span className="text-c600 mx-2">
-                    <i className="fas fa-comments mr-1 text-c500"></i>2 Comments
-                  </span>
-                  <h4 className="text-c100 font-bold">Water Need in Africa</h4>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <News />
         <section className="sponsors p-0 border-t sponser-border__top">
           <div className="container">
             <div className="flex justify-between mx-auto my-32 flex-wrap">
