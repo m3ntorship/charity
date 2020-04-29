077b666b0341e82d511f47e2aa86be91f07aaaa0
zahraa333 <zahraa.khalil2020@gmail.com>
2020-04-30 00:34:15 +0200

    delete svg imgs & link components with api

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,102 +1,72 @@
 import React, { Component } from 'react';
-// import { charityAPI } from '../../clients';
+import { charityAPI } from '../../clients';
 import { Fragment } from 'react';
 import './styles.css';
 
-const data = {
-  welcome_image:
-    'https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg',
-  header: 'This is welcome header',
-  title_complementary: 'complementary',
-  description: 'this is welcome description',
-  mini_cards: [
-    {
-      id: 1,
-      card_title: 'Become a volunteer',
-      card_desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-    },
-    {
-      id: 2,
-      card_title: 'Become a volunteer',
-      card_desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-    }
-  ],
-  card_btn: 'Discover More',
-  loading: true,
-  error: false
-};
-
-function charityAPIData() {
-  return new Promise(() => {
-    return data; 
-  });
-}
-
 export default class Welcome extends Component {
   state = {
-    welcome_image:
-      'https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg',
-    header: 'This is welcome header',
-    title_complementary: 'complementary',
-    description: 'this is welcome description',
-    mini_cards: [
-      {
-        id: 1,
-        card_title: 'Become a volunteer',
-        card_desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-      },
-      {
-        id: 2,
-        card_title: 'Become a volunteer',
-        card_desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-      }
-    ],
-    card_btn: 'Discover More',
+    heading_primary: null,
+    heading_secondary: null,
+    main_image_src: null,
+    description: null,
+    mini_cards: [],
+    call_to_action_text: null,
+    call_to_action_url: null,
     loading: true,
     error: false
   };
 
-  // mount plz
-
-  //  componentDidMount() {
-  //   charityAPIData()
-  //     .then(({ data:  }) => {
-  //       this.setState({
-  //         activities_cards: activities.how_we_work_cards,
-  //         description: activities.description,
-  //         title_primary: activities.title_primary,
-  //         title_complementary: activities.title_complementary,
-  //         error: false,
-  //         loading: false
-  //       });
-  //     })
-  //     .catch(error => {
-  //       this.setState({
-  //         error: true,
-  //         loading: false
-  //       });
-  //     });
-  // }
+  componentDidMount() {
+    charityAPI('/welcome-section')
+      .then(
+        ({
+          data: {
+            Heading: { heading_primary, heading_secondary },
+            WelcomeActions: mini_cards,
+            link: { text: call_to_action_text, url: call_to_action_url },
+            image: { url: main_image_src },
+            description
+          }
+        }) => {
+          this.setState({
+            heading_primary,
+            heading_secondary,
+            main_image_src,
+            description,
+            mini_cards,
+            call_to_action_text,
+            call_to_action_url,
+            loading: false,
+            error: false
+          });
+        }
+      )
+      .catch(error => {
+        this.setState({
+          error: true,
+          loading: false
+        });
+      });
+  }
 
   render() {
-    console.log(charityAPIData());
     return (
       <Fragment>
         <section className="welcome py-0 text-c600">
           <div className="container grid grid-cols-12 gap-8">
-            <WelcomeImage url={this.state.welcome_image} />
+            <WelcomeImage url={this.state.main_image_src} />
 
             <div className="welcome__end col-start-7 col-end-13 pt-16">
               <WelcomeHeader
-                header={this.state.header}
-                title_complementary={this.state.title_complementary}
+                header={this.state.heading_primary}
+                title_complementary={this.state.heading_secondary}
                 desc={this.state.description}
               />
               <ul className="flex welcome__list">
                 <MiniCard cardInfo={this.state.mini_cards} />
               </ul>
 
-              <WelcomeBtn button={this.state.card_btn} />
+              <WelcomeBtn textUrl={this.state.call_to_action} />
             </div>
           </div>
         </section>
@@ -162,41 +132,48 @@ class MiniCard extends Component {
 class WelcomeBtn extends Component {
   render() {
     return (
-      <button className="btn-lg bg-c300 mt-16">{this.props.button}</button>
+      <button
+        href={this.props.call_to_action_url}
+        className="btn-lg bg-c300 mt-16"
+      >
+        {this.props.call_to_action_text}
+      </button>
     );
   }
 }
 
-// <div className="welcome__end col-start-7 col-end-13 pt-16">
+{
+  /* <div className="welcome__end col-start-7 col-end-13 pt-16">
 
-//   <h2 className="text-c100 leading-tighter tracking-tight font-extrabold my-12">
-//     Welcome to Best Charity
-//     <span className="text-c200 font-hairline underline border-b-2">
-//       Organization
-//     </span>
-//   </h2>
+  <h2 className="text-c100 leading-tighter tracking-tight font-extrabold my-12">
+    Welcome to Best Charity
+    <span className="text-c200 font-hairline underline border-b-2">
+      Organization
+    </span>
+  </h2>
 
-//   <p>
-//     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quaerat
-//     similique corrupti autem repellendus at aliquid ab? Soluta est doloremque
-//     repellat voluptas aperiam necessitatibus eligendi.
-//   </p>
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga quaerat
+    similique corrupti autem repellendus at aliquid ab? Soluta est doloremque
+    repellat voluptas aperiam necessitatibus eligendi.
+  </p>
 
-//   <ul className="flex welcome__list">
-//     <li className="welcome__list__item pl-4">
-//       <h3 className="welcome__list__item__title relative my-10 text-md font-bold text-c100">
-//         Become a Volunteer
-//       </h3>
-//       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-//     </li>
+  <ul className="flex welcome__list">
+    <li className="welcome__list__item pl-4">
+      <h3 className="welcome__list__item__title relative my-10 text-md font-bold text-c100">
+        Become a Volunteer
+      </h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </li>
 
-//     <li className="welcome__list__item pl-4">
-//       <h3 className="welcome__list__item__title relative my-10 text-md font-bold text-c100">
-//         Quick Fundraising
-//       </h3>
-//       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-//     </li>
-//   </ul>
+    <li className="welcome__list__item pl-4">
+      <h3 className="welcome__list__item__title relative my-10 text-md font-bold text-c100">
+        Quick Fundraising
+      </h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+    </li>
+  </ul>
 
-//   <button className="btn-lg bg-c300 mt-16">Discover More</button>
-// </div>;
+  <button className="btn-lg bg-c300 mt-16">Discover More</button>
+</div>; */
+}
