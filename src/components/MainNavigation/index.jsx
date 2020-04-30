c5027303f4fadb8fe49f8525571b493aae531dd2
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-04-30 04:31:18 +0200

    done condtional rendering based on the fetched data, loopped over all the data array & created NavigationLink Component for each data object

--- src/components/MainNavigation/index.jsx
+++ src/components/MainNavigation/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { charityAPI } from '../../clients';
+import NavigationLink from '../NavigationLink';
 import './style.css';
 
 export default class MainNavigation extends React.Component {
-  state = { mainNavigation: {}, loading: true, error: false, errorMSG: '' };
+  state = { mainNavigation: [], loading: true, error: false, errorMSG: '' };
 
   componentDidMount() {
     charityAPI('/main-navigation')
@@ -20,35 +21,24 @@ export default class MainNavigation extends React.Component {
   }
 
   render() {
-    return (
-      <nav className="flex">
-        <ul className="flex justify-around w-8/12 bg-c200 mr-2 items-center text-c000">
-          <li>
-            <a href="#a">Home</a>
-          </li>
-          <li>
-            <a href="#a">Pages</a>
-          </li>
-          <li>
-            <a href="#a">Donations</a>
-          </li>
-          <li>
-            <a href="#a">Events</a>
-          </li>
-          <li>
-            <a href="#a">News</a>
-          </li>
-          <li>
-            <a href="#a">Contact</a>
-          </li>
-        </ul>
-        <a
-          className="btn-md w-2/6 bg-c300 flex items-center justify-center"
-          href="#a"
-        >
-          Start Donating
-        </a>
-      </nav>
-    );
+    if (this.state.error && !this.state.loading) {
+      return <div>{this.state.errorMSG}</div>;
+    } else {
+      return (
+        <nav className="flex">
+          <ul className="flex justify-around w-8/12 bg-c200 mr-2 items-center text-c000">
+            {this.state.mainNavigation.map(linkObj => (
+              <NavigationLink title={linkObj.title} url={linkObj.url} />
+            ))}
+          </ul>
+          <a
+            className="btn-md w-2/6 bg-c300 flex items-center justify-center"
+            href="#a"
+          >
+            Start Donating
+          </a>
+        </nav>
+      );
+    }
   }
 }
