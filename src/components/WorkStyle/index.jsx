a5730e4da8766a56fd3716352fd09c2a0da1308e
AbdoMagdy <abdoman46@hotmail.com>
2020-04-30 15:30:34 +0200

    handle errors in a better wat

--- src/components/WorkStyle/index.jsx
+++ src/components/WorkStyle/index.jsx
@@ -5,24 +5,50 @@ import { charityAPI } from '../../clients';
 import './styles.css';
 
 export default class WorkStyle extends React.Component {
-  constructor() {
-    super();
-    this.state = { Work: {} };
-  }
+  state = {
+    data: {},
+    loading: true,
+    error: false,
+    errorMessage: ''
+  };
 
   componentDidMount() {
-    charityAPI({
-      url: '/how-we-work'
-    })
-      .then(({ data: Work }) => {
-        this.setState({ Work });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    this.setState({ loading: true });
+    this._getData();
   }
+  _getData = () => {
+    charityAPI('/popular-causes')
+      .then(({ data }) => {
+        this.setState({
+          data: data,
+          loading: false,
+          error: false
+        });
+      })
+      .catch(error =>
+        this.setState({
+          error: true,
+          loading: false,
+          errorMessage: " Couldn't fetch data"
+        })
+      );
+  };
 
   render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (this.state.error) {
+      return (
+        <div>
+          {this.state.errorMessage},{' '}
+          <a href="#/" onClick={this._getData} className="text-c200">
+            retry?
+          </a>
+        </div>
+      );
+    }
     return (
       <section
         className="work-style relative text-c600"
@@ -31,14 +57,14 @@ export default class WorkStyle extends React.Component {
         <div className="container">
           <Heading
             primaryTextColor="dark"
-            primaryText={this.state.Work.title_primary}
-            secondaryText={this.state.Work.title_complementary}
+            primaryText={this.state.data.title_primary}
+            secondaryText={this.state.data.title_complementary}
           />
 
           <div className="work-style__items mx-auto showcase-row flex-col items-center md:flex-row md:items-start">
             {(() => {
-              if (this.state.Work.Cards) {
-                return this.state.Work.Cards.map(card => (
+              if (this.state.data.Cards) {
+                return this.state.data.Cards.map(card => (
                   <WorkStyleCard
                     description={card.description}
                     title={card.Title}
