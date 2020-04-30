b1bff5681374eef770ee198af684846eca5b2aba
Mohamed Mahsoub <56096335+Mohmmed-Mahsoub@users.noreply.github.com>
2020-04-30 16:42:42 +0200

    fetch data from Strapi and import CSS styles

--- src/components/sponsers/index.jsx
+++ src/components/sponsers/index.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
-//import { charityAPI } from '../../clients';
-//import sponserImg from './img/envato-dark.png';
-const charityAPI = () =>
-  Promise.resolve({
-    data: [
-      { url: require('./img/envato-dark.png') },
-      { url: require('./img/envato-dark.png') },
-      { url: require('./img/envato-dark.png') },
-      { url: require('./img/envato-dark.png') }
-    ]
-  });
+import { charityAPI } from '../../clients';
+import './styles.css';
+
 class Sponsers extends React.Component {
   state = { urls: [], urlsError: false };
   componentDidMount() {
-    charityAPI('/urls')
-      .then(({ data: urls }) => {
-        this.setState({ urls });
+    charityAPI('/Sponsers')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          urls: res.data
+        });
       })
       .catch(error => {});
   }
   render() {
-    return this.state.urls.map(({ url }) => (
-      <div className="sponser">
-        <img src={url} alt="sponser" />
+    return this.state.urls.map(({ _id, image }) => (
+      <div className="sponser" key={_id}>
+        <img src={image.url} alt="sponser" />
       </div>
     ));
   }
