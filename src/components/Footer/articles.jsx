f36240502c8cc745f029dd313d465c8fca273653
AbdoMagdy <abdoman46@hotmail.com>
2020-05-01 01:35:33 +0200

    used date-fns

--- src/components/Footer/articles.jsx
+++ src/components/Footer/articles.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-
+import { parseISO, format } from 'date-fns';
 export default class Articles extends React.Component {
+  getDate(myDate) {
+    const theDate = parseISO(myDate);
+
+    return {
+      time: format(theDate, 'hh:mm a').toLowerCase(),
+      day: format(theDate, 'dd'),
+      month: format(theDate, 'MMM'),
+      year: format(theDate, 'yyyy')
+    };
+  }
+
   render() {
     const articles = this.props.articles.map(article => {
+      const formattedDate = this.getDate(article.date);
       return (
         <div className="flex flex-col flex-grow">
-          <a className="hover:text-c000 mb-8" href={article.link}>
+          <a className="hover:text-c000 mb-8" href={article.link.url}>
             <div className="flex">
               <div className="news__image-container">
                 <img
                   width="72"
                   height="72"
-                  src={article.img}
-                  alt={article.description}
+                  src={article.image.url}
+                  alt={article.image.alternativeText}
                 />
               </div>
               <div className="pl-4 flex flex-col justify-between">
-                <p className="text-c300 text-xs">{article.date}</p>
+                <p className="text-c300 text-xs">
+                  {formattedDate.day}-{formattedDate.month}-{formattedDate.year}
+                </p>
                 <p>{article.description}</p>
               </div>
             </div>
