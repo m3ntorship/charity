0e35aacf6b5402d60fcb5cde164a2c8cdb7b489b
malngaawy <mohager477@gmail.com>
2020-04-30 02:04:29 +0200

    separate events component

--- src/components/UpcomingEvents/index.jsx
+++ src/components/UpcomingEvents/index.jsx
@@ -11,7 +11,43 @@ export const BackgroundImage = () => (
   </div>
 );
 
-const 
+const Events = props => {
+  return (
+    <div>
+      {props.data.map(({ id, title, address, date, image: { url, name } }) => (
+        <div key={id} className="event-card-wrapper flex w-4/5 mb-4">
+          <div className="event-card-wrapper_image">
+            <img src={url} alt={name} />
+          </div>
+          <div className="event-card-wrapper__details relative self-center pl-16">
+            <div className="event-card-wrapper__timing text-base text-c200 flex items-center leading-relaxed">
+              <span className="tracking-wide">admin</span>
+              &nbsp; &nbsp;
+              <span>12:00 am</span>
+            </div>
+            <div className="event-card-wrapper__topic">
+              <p className="text-c100 font-bold text-lg leading-relaxed">
+                {title}
+              </p>
+            </div>
+            <div className="event-card-wrapper__location">
+              <p className="text-c600 text-base flex items-center leading-relaxed">
+                {address}
+              </p>
+            </div>
+            <div className="event-card-wrapper__date">
+              <div className="event_date">
+                <span> 07</span>
+                <span> Feb</span>
+                <span>2017</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export const UpcomingEvents = () => {
   const [data, setData] = useState(null);
@@ -24,8 +60,6 @@ export const UpcomingEvents = () => {
         setData(data);
         setFetchingDataError(false);
         setLoading(false);
-        // const d = new Date(data.upcoming_events[0].date);
-        // console.log(d.split(' '));
       })
       .catch(error => {
         setFetchingDataError(error);
@@ -52,39 +86,7 @@ export const UpcomingEvents = () => {
       <p className="upcoming-events_description text-c600 leading-loose mb-12">
         {data.description}
       </p>
-      {data.upcoming_events.map(
-        ({ id, title, address, date, image: { url, name } }) => (
-          <div key={id} className="event-card-wrapper flex w-4/5 mb-4">
-            <div className="event-card-wrapper_image">
-              <img src={url} alt={name} />
-            </div>
-            <div className="event-card-wrapper__details relative self-center pl-16">
-              <div className="event-card-wrapper__timing text-base text-c200 flex items-center leading-relaxed">
-                <span className="tracking-wide">admin</span>
-                &nbsp; &nbsp;
-                <span>12:00 am</span>
-              </div>
-              <div className="event-card-wrapper__topic">
-                <p className="text-c100 font-bold text-lg leading-relaxed">
-                  {title}
-                </p>
-              </div>
-              <div className="event-card-wrapper__location">
-                <p className="text-c600 text-base flex items-center leading-relaxed">
-                  {address}
-                </p>
-              </div>
-              <div className="event-card-wrapper__date">
-                <div className="event_date">
-                  <span> 07</span>
-                  <span> Feb</span>
-                  <span>2017</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        )
-      )}
+      <Events data={data.upcoming_events} />
     </div>
   );
 };
