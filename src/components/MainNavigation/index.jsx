42d43fa76804a0482d7f79039a6269cf34503dbe
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-04-30 17:28:22 +0200

    fixed fetching data issues

--- src/components/MainNavigation/index.jsx
+++ src/components/MainNavigation/index.jsx
@@ -4,31 +4,42 @@ import NavigationLink from '../NavigationLink';
 import './style.css';
 
 export default class MainNavigation extends React.Component {
-  state = { mainNavigation: [], loading: true, error: false, errorMSG: '' };
+  state = {
+    mainNavigation: [],
+    loading: true,
+    error: false,
+    errorMSG: ''
+  };
 
   componentDidMount() {
     charityAPI('/main-navigation')
-      .then(({ data: mainNavigation }) => {
-        this.setState({ mainNavigation });
+      .then(data => {
+        this.setState({
+          mainNavigation: data.data.Links,
+          loading: false
+        });
       })
       .catch(error => {
         this.setState({
-          loading: false,
           error: true,
-          errorMSG: 'Could not fetch the required data!'
+          loading: false
         });
       });
   }
 
   render() {
-    if (this.state.error && !this.state.loading) {
+    if (this.state.error) {
       return <div>{this.state.errorMSG}</div>;
     } else {
       return (
         <nav className="flex">
           <ul className="flex justify-around w-8/12 bg-c200 mr-2 items-center text-c000">
             {this.state.mainNavigation.map(linkObj => (
-              <NavigationLink title={linkObj.title} url={linkObj.url} />
+              <NavigationLink
+                title={linkObj.text}
+                url={linkObj.url}
+                key={linkObj.id}
+              />
             ))}
           </ul>
           <a
