964d40db7fb52c264da1167319c8fa6034698673
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-05-01 02:33:14 +0200

    handle loading and error states

--- src/components/NewsAndArticles/index.jsx
+++ src/components/NewsAndArticles/index.jsx
@@ -9,7 +9,10 @@ class News extends Component {
       description: '',
       link: {},
       articles: []
-    }
+    },
+    loading: false,
+    error: false,
+    errorMessage: ''
   };
 
   componentDidMount() {
@@ -17,6 +20,8 @@ class News extends Component {
   }
 
   _getData = () => {
+    this.setState({ loading: true, error: false });
+
     charityAPI('/news-and-articles')
       .then(({ data: { heading, description, link, home_articles } }) => {
         this.setState({
@@ -25,11 +30,17 @@ class News extends Component {
             description,
             link,
             articles: home_articles
-          }
+          },
+          loading: false,
+          error: false
         });
       })
       .catch(err => {
-        console.log(err);
+        this.setState({
+          loading: false,
+          error: true,
+          errorMessage: 'An Error occurred while getting the data, '
+        });
       });
   };
 
@@ -58,36 +69,51 @@ class News extends Component {
       )
     );
 
-    return (
-      <section className="news font-body bg-c800 mb-20 md:mb-48 pt-18 pb-1 md:pb-40 relative">
-        <div className="container">
-          <div className="head-section grid grid-cols-1 md:grid-cols-12">
-            <h2 className="text-c100 font-black md:col-span-5">
-              {heading_primary}
-              <span className="text-c200 font-thin border-b-2">
-                {heading_secondary}
-              </span>
-            </h2>
-            <p className="text-c600 text-lg md:mt-3 md:col-span-4 leading-relaxed">
-              {this.state.data.description}
-            </p>
-            <div className="btn-div md:col-span-3">
-              <a
-                className="btn-sm text-sm bg-c300 my-8 md:float-right md:mt-3 cursor-pointer"
-                href={url}
-              >
-                {text}
-              </a>
+    if (this.state.loading) {
+      return <div>loading data.....</div>;
+    } else if (this.state.error) {
+      return (
+        <div>
+          <p>
+            {this.state.errorMessage}
+            <span className="cursor-pointer text-c200" onClick={this._getData}>
+              Retry?
+            </span>
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <section className="news font-body bg-c800 mb-20 md:mb-48 pt-18 pb-1 md:pb-40 relative">
+          <div className="container">
+            <div className="head-section grid grid-cols-1 md:grid-cols-12">
+              <h2 className="text-c100 font-black md:col-span-5">
+                {heading_primary}
+                <span className="text-c200 font-thin border-b-2">
+                  {heading_secondary}
+                </span>
+              </h2>
+              <p className="text-c600 text-lg md:mt-3 md:col-span-4 leading-relaxed">
+                {this.state.data.description}
+              </p>
+              <div className="btn-div md:col-span-3">
+                <a
+                  className="btn-sm text-sm bg-c300 my-8 md:float-right md:mt-3 cursor-pointer"
+                  href={url}
+                >
+                  {text}
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="container relative">
-          <div className="articles grid grid-cols-1 mt-12 md:mt-auto md:grid-cols-3 gap-8 md:absolute w-full">
-            {articlesList}
+          <div className="container relative">
+            <div className="articles grid grid-cols-1 mt-12 md:mt-auto md:grid-cols-3 gap-8 md:absolute w-full">
+              {articlesList}
+            </div>
           </div>
-        </div>
-      </section>
-    );
+        </section>
+      );
+    }
   }
 }
 
