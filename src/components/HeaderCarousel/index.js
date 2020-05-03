b46b28dac5191099cf368eee27a932378df1f899
Tramsi <tramsi2014@gmail.com>
2020-05-03 12:37:28 +0200

    - Added pure-react-carousel - Main functionality of carousel

--- src/components/HeaderCarousel/index.js
+++ src/components/HeaderCarousel/index.js
@@ -5,58 +5,80 @@ import left_arrow_icon from './img/left-arrow-icon.png';
 import right_arrow_icon from './img/right-arrow-icon.png';
 import plant_left from './img/plant-left.png';
 import plant_right from './img/plant-right.png';
+import {
+  CarouselProvider,
+  Slide,
+  Slider,
+  ButtonBack,
+  ButtonNext
+} from 'pure-react-carousel';
+const HeaderCarousel = () => {
+  return (
+    <section className="slider py-0 bg-cover bg-center bg-c800 relative">
+      <div className="container">
+        <MainNavigation />
+      </div>
 
-const Carousel = () => {
-  return(
-    <section className="slider pt-0 bg-cover bg-center relative bg-c800 relative">
-      <div className="overlay absolute h-full w-full">
-        <div className="container h-full flex flex-col">
-          <MainNavigation />
-
-          <div className="header-slider h-full flex items-center justify-center">
-            <div className="text text-center text-c000">
-              <p className="text-lg italic">Make an Impact</p>
-              <div className="main flex items-center justify-center">
-                <img
-                  src={plant_left}
-                  alt="left-spike"
-                />
-                <p className="font-bold leading-none">
-                  You Can Make <br />A
-                  <span className="underline text-c300 font-hairline">
-                    Difference
-                  </span>
-                </p>
-                <img
-                  src={plant_right}
-                  alt="right-spike"
-                />
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={45}
+        totalSlides={2}
+        className="h-full"
+      >
+        <Slider className="h-full">
+          <Slide className="h-full">
+            <div className="header-slider h-full flex items-center justify-center">
+              <div className="text text-center text-c000">
+                <p className="text-lg italic">Make an Impact</p>
+                <div className="main flex items-center justify-center">
+                  <img src={plant_left} alt="left-spike" />
+                  <p className="font-bold leading-none">
+                    You Can Make <br />A
+                    <span className="underline text-c300 font-hairline">
+                      Difference
+                    </span>
+                  </p>
+                  <img src={plant_right} alt="right-spike" />
+                </div>
+                <a className="btn-md bg-c200 inline-block mt-6" href="#a">
+                  View Causes
+                </a>
               </div>
-              <a className="btn-md bg-c200 inline-block mt-6" href="#a">
-                View Causes
-              </a>
             </div>
-          </div>
-        </div>
+          </Slide>
+          <Slide className="h-full">
+            <div className="header-slider h-full flex items-center justify-center">
+              <div className="text text-center text-c000">
+                <p className="text-lg italic">Make an Impact</p>
+                <div className="main flex items-center justify-center">
+                  <img src={plant_left} alt="left-spike" />
+                  <p className="font-bold leading-none">
+                    You Can Make <br />A
+                    <span className="underline text-c300 font-hairline">
+                      Difference
+                    </span>
+                  </p>
+                  <img src={plant_right} alt="right-spike" />
+                </div>
+                <a className="btn-md bg-c200 inline-block mt-6" href="#a">
+                  View Causes
+                </a>
+              </div>
+            </div>
+          </Slide>
+        </Slider>
 
         <div className="arrows absolute w-full flex justify-between px-16">
-          <button className="left-arrow rounded-full border-solid border-c000 border p-4 opacity-50">
-            <img
-              src={left_arrow_icon}
-              alt="left-arrow-icon"
-            />
-          </button>
-          <button className="right-arrow rounded-full border-solid border-c000 border p-4">
-            <img
-              src={right_arrow_icon}
-              alt="right-arrow-icon"
-            />
-          </button>
+          <ButtonBack className="right-arrow rounded-full border-solid border-c000 bg-c300 border p-4">
+            <i className="fas fa-arrow-left"></i>
+          </ButtonBack>
+          <ButtonNext className="right-arrow rounded-full border-solid border-c000 bg-c300 border p-4">
+            <i className="fas fa-arrow-right"></i>
+          </ButtonNext>
         </div>
-      </div>
+      </CarouselProvider>
     </section>
-  )
-}
+  );
+};
 
-
-export {Carousel}
+export default HeaderCarousel;
