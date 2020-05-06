b2f492b32afc9dc385c784f54415d0f677d08f23
elhaw <abd.elsayed@hotmail.com>
2020-05-07 03:23:12 +0400

    updated config overides config

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,33 @@
 const purgecss = require('@fullhuman/postcss-purgecss');
-
+const isProductionMode =
+  process.env.NODE_ENV === 'ci' || process.env.NODE_ENV === 'production';
 module.exports = {
   webpack: function (config, env) {
     require('react-app-rewire-postcss')(config, {
-      plugins: loader => [
-        require('postcss-import'),
-        require('postcss-extend'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        purgecss({
-          content: [
-            './public/**/*.html',
-            './src/**/*.html',
-            './src/**/*.jsx',
-            './src/**/*.js'
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      ]
+      plugins: () => {
+        let plugins = [
+          require('postcss-import'),
+          require('postcss-extend'),
+          require('tailwindcss'),
+          require('autoprefixer')
+        ];
+
+        if (isProductionMode) {
+          plugins.push(
+            purgecss({
+              content: [
+                './public/**/*.html',
+                './src/**/*.html',
+                './src/**/*.jsx',
+                './src/**/*.js'
+              ],
+              defaultExtractor: content =>
+                content.match(/[\w-/:]+(?<!:)/g) || []
+            })
+          );
+        }
+        return plugins;
+      }
     });
     return config;
   }
