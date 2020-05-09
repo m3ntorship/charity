633fafedb58d4b6d2c770a93548511317feadcda
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-05-09 16:49:49 +0200

    made ContactInfo resposive

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -38,50 +38,58 @@ export const ContactInfo = () => {
 
   return (
     <section className="contact-section py-10">
-        <div className="container">
-          <div className="flex justify-around items-center mx-0 text-c000">
-              <div className="contact-section__social  flex justify-start text-xxs">
-                {socialData.map(({ _id, url, fontawesome_icons }) => (
-                  <div
-                    key={_id}
-                    className="contact-section__social-icon hover:bg-c200 ml-0"
-                  >
-                    <a className="full-width-click" href={url} target="_blank" rel="noopener noreferrer" >
-                      <i className={fontawesome_icons}></i>
-                    </a>
-                  </div>
-                ))}
+      <div className="container">
+        <div className="flex justify-around items-center mx-0 text-c000">
+          <div className="contact-section__social  flex justify-start text-xxs">
+            {socialData.map(({ _id, url, fontawesome_icons }) => (
+              <div
+                key={_id}
+                className="contact-section__social-icon hover:bg-c200 ml-0"
+              >
+                <a
+                  className="full-width-click"
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className={fontawesome_icons}></i>
+                </a>
               </div>
+            ))}
+          </div>
 
-              <div className="contact-section__contacts  flex flex-wrap justify-end text-sm">
-                {contactData.map(
-                  ({ _id, title,url, sub_title, icon }, index) => {
-                    const isLast = index === contactData.length - 1;
-                    return (
-                      <div
-                        key={_id}
-                        className={cn('contact', {
-                          'border-right': !isLast
-                        })}
-                      >
-                        <div className="contact-icon">
-                          <img className="h-auto" src={icon.url} alt={icon.name} />
-                        </div>
-                        <div className="information">
-                          <a className="block" href= {url} target = "_blank" rel="noopener noreferrer" >
-                            {title}
-                          </a>
-                          <small className="information-small leading-normal">
-                            {sub_title}
-                          </small>
-                        </div>
-                      </div>
-                    );
-                  }
-                )}
-              </div>
+          <div className="contact-section__contacts flex flex-wrap flex-col lg:flex-row justify-end text-sm mx-6">
+            {contactData.map(({ _id, title, url, sub_title, icon }, index) => {
+              const isLast = index === contactData.length - 1;
+              return (
+                <div
+                  key={_id}
+                  className={cn('contact', 'my-1', 'lg:my-0', {
+                    'border-right': !isLast
+                  })}
+                >
+                  <div className="contact-icon">
+                    <img className="h-auto" src={icon.url} alt={icon.name} />
+                  </div>
+                  <div className="information">
+                    <a
+                      className="block"
+                      href={url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {title}
+                    </a>
+                    <small className="information-small leading-normal">
+                      {sub_title}
+                    </small>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
+      </div>
     </section>
   );
 };
