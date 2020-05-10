da5b583d4a78e1017174282ec482fb5dfacb5bb1
Tramsi <tramsi2014@gmail.com>
2020-05-10 13:11:38 +0200

    Imported useState and useEffect in useMedia file

--- src/components/Causes/useMedia.jsx
+++ src/components/Causes/useMedia.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 function useMedia(queries, values, defaultValue) {
   // Array containing a media query list for each query
   const mediaQueryLists = queries.map(q => window.matchMedia(q));
