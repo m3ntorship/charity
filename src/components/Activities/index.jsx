3a78f95401004bffff886c14b395186c22fd7d12
elhaw <abd.elsayed@hotmail.com>
2020-05-11 17:15:08 +0400

    removed ContentLoader component

--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ContentLoader from 'react-content-loader';
 import {CardLoader, TitleLoader} from './ActivitiesContentLoading/index'
 import { charityAPI } from '../../clients';
 import { Fragment } from 'react';
