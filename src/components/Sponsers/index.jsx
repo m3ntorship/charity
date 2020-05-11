d9e0db91ec28eae1eb191f420af797b45d7eadf8
malngaawy <mohager477@gmail.com>
2020-05-11 14:18:18 +0200

    add loader to spoonsers

--- src/components/Sponsers/index.jsx
+++ src/components/Sponsers/index.jsx
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { charityAPI } from '../../clients';
 import './styles.css';
+import ContentLoader from 'react-content-loader';
+
+const MyLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={100}
+    height={100}
+    viewBox="0 0 100 100"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#e8e8e8"
+    className="m-auto"
+  >
+    <circle cx="53" cy="50" r="33" />
+  </ContentLoader>
+);
 
 const Sponsers = () => {
   const [urls, setUrls] = useState([]);
@@ -19,8 +34,20 @@ const Sponsers = () => {
         setLoading(false);
       });
   });
+
   if (loading) {
-    return <div>loading data </div>;
+    return (
+      <section className="sponsors p-0 border-t sponser-border__top">
+        <div className="container">
+          <div className="mx-auto my-32 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+            <MyLoader />
+            <MyLoader />
+            <MyLoader />
+            <MyLoader />
+          </div>
+        </div>
+      </section>
+    );
   }
   if (error) {
     return <div>we can not fetch data</div>;
