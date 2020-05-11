6eb92e3a837c653e5fcf0775021c8b7d6f21811e
elhaw <abd.elsayed@hotmail.com>
2020-05-11 23:33:29 +0400

    added footer loading content

---
+++ src/components/Footer/FooterContentLoading/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+export const AboutLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={350}
+    viewBox="0 0 230 360"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="20" rx="0" ry="0" width="80" height="30" />
+    <rect x="0" y="100" rx="0" ry="0" width="116" height="4" />
+    <rect x="0" y="110" rx="0" ry="0" width="116" height="4" />
+    <rect x="0" y="120" rx="0" ry="0" width="116" height="4" />
+    <rect x="0" y="130" rx="0" ry="0" width="116" height="4" />
+    <rect x="0" y="140" rx="0" ry="0" width="116" height="4" />
+
+    <rect x="0" y="200" rx="0" ry="0" width="140" height="50" />
+  </ContentLoader>
+);
+
+export const ArticlesLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={350}
+    viewBox="0 0 230 360"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="20" rx="0" ry="0" width="80" height="30" />
+    <rect x="0" y="100" rx="0" ry="0" width="80" height="80" />
+    <rect x="120" y="110" rx="0" ry="0" width="100" height="4" />
+    <rect x="120" y="125" rx="0" ry="0" width="100" height="4" />
+    <rect x="120" y="140" rx="0" ry="0" width="100" height="4" />
+
+    <rect x="0" y="200" rx="0" ry="0" width="80" height="80" />
+    <rect x="120" y="215" rx="0" ry="0" width="100" height="4" />
+    <rect x="120" y="230" rx="0" ry="0" width="100" height="4" />
+    <rect x="120" y="245" rx="0" ry="0" width="100" height="4" />
+  </ContentLoader>
+);
+
+export const LinksLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={350}
+    viewBox="0 0 230 360"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="20" rx="0" ry="0" width="60" height="30" />
+    <rect x="100" y="20" rx="0" ry="0" width="60" height="30" />
+    {/* <rect x="30" y="20" rx="0" ry="0" width="60" height="30" />
+    <rect x="30" y="40" rx="0" ry="0" width="60" height="30" />
+    <rect x="60" y="20" rx="0" ry="0" width="60" height="30" />
+    <rect x="60" y="40" rx="0" ry="0" width="60" height="30" /> */}
+  </ContentLoader>
+);
+
+export const NewsletterLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={350}
+    viewBox="0 0 230 360"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="20" rx="0" ry="0" width="80" height="30" />
+    <rect x="0" y="100" rx="0" ry="0" width="150" height="40" />
+    <rect x="0" y="150" rx="0" ry="0" width="150" height="40" />
+  </ContentLoader>
+);
