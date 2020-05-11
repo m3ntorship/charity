71ec1838e2444d6674718a56e042a044fd6bb91d
elhaw <abd.elsayed@hotmail.com>
2020-05-11 17:10:01 +0400

    chaned css class in responsive

--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -1,39 +1,10 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
-
+import {CardLoader, TitleLoader} from './ActivitiesContentLoading/index'
 import { charityAPI } from '../../clients';
 import { Fragment } from 'react';
 import './styles.css';
 
-const CardLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={230}
-    height={410}
-    viewBox="0 0 230 410"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
-    <circle cx="117" cy="117" r="50" />
-    <rect x="51" y="246" rx="0" ry="0" width="150" height="10" />
-    <rect x="64" y="347" rx="0" ry="0" width="122" height="4" />
-    <rect x="85" y="316" rx="0" ry="0" width="83" height="4" />
-    <rect x="84" y="374" rx="0" ry="0" width="83" height="4" />
-  </ContentLoader>
-);
-
-const TitleLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={400}
-    height={115}
-    viewBox="0 0 250 50"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
-    <rect x="0" y="0" rx="0" ry="0" width="400" height="115" />
-  </ContentLoader>
-);
 
 export default class Activities extends React.Component {
   constructor(props) {
@@ -93,7 +64,7 @@ export default class Activities extends React.Component {
     if (this.state.loading) {
       return (
         <div className="container activites px-5 py-5">
-          <div className="flex flex-wrap  md:justify-between   ">
+          <div className="flex flex-wrap justify-center  lg:justify-between   ">
             <TitleLoader />
             <TitleLoader />
           </div>
