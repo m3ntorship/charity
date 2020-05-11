93abc531b0ddacc49d9f0f4c82724be4387dd1eb
AbdoMagdy <abdoman46@hotmail.com>
2020-05-11 07:17:03 +0200

    transtion between content-loader and contactTop

--- src/components/ContactTop/index.jsx
+++ src/components/ContactTop/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useTransition, animated } from 'react-spring';
 import { charityAPI } from '../../clients/charity';
-
 import './style.scss';
 import Loader from './ContentLoader';
 const ContactTop = () => {
@@ -26,15 +26,17 @@ const ContactTop = () => {
         });
       });
   };
-
   useEffect(() => {
-    setLoadingState(true);
     getData();
   }, []);
-  if (loadingState) {
-    return <Loader style={{ width: '100%', height: 'auto' }} />;
-  }
-
+  const transitions = useTransition(loadingState, item => item.id, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: {
+      duration: 1000
+    }
+  });
   if (errorState.error) {
     return (
       <div>
@@ -44,35 +46,42 @@ const ContactTop = () => {
         </a>
       </div>
     );
-  } else {
-    return (
-      <section className="contact-top px-8 bg-c100 py-4 hidden md:block">
-        <div className="container hidden md:flex justify-between">
-          <div className="welcome-text text-sm">
-            Welcome to the best
-            <span className="text-c300 underline italic">Lovims</span> charity
-            platform
-          </div>
-          <div className="social flex text-sm">
-            <div>Follow us:</div>
-            <div className="ml-6">
-              <ul className="inline-block ">
-                {dataState.map(item => {
-                  return (
-                    <li key={item.id} className="inline px-3 hover:text-c000">
-                      <a href={item.url}>
-                        <i className={item.fontawesome_icons}></i>
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
+  }
+  console.log(loadingState);
+  return transitions.map(({ item, key, props }) =>
+    item ? (
+      <animated.div style={props}>
+        <Loader style={{ width: '100%', height: 'auto' }} />
+      </animated.div>
+    ) : (
+      <animated.div style={props}>
+        <section className="contact-top px-8 bg-c100 py-4 hidden md:block">
+          <div className="container hidden md:flex justify-between">
+            <div className="welcome-text text-sm">
+              Welcome to the best
+              <span className="text-c300 underline italic">Lovims</span> charity
+              platform
+            </div>
+            <div className="social flex text-sm">
+              <div>Follow us:</div>
+              <div className="ml-6">
+                <ul className="inline-block ">
+                  {dataState.map(item => {
+                    return (
+                      <li key={item.id} className="inline px-3 hover:text-c000">
+                        <a href={item.url}>
+                          <i className={item.fontawesome_icons}></i>
+                        </a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    );
-  }
+        </section>
+      </animated.div>
+    )
+  );
 };
-
 export default ContactTop;
