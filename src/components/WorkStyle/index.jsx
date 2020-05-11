6342e46b884775e3833c266850a409e1533e9e3f
elhaw <abd.elsayed@hotmail.com>
2020-05-11 12:43:45 +0400

    added content loader for how we work section

--- src/components/WorkStyle/index.jsx
+++ src/components/WorkStyle/index.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import ContentLoader from 'react-content-loader';
 import WorkStyleCard from '../WorkStyleCard';
 import Heading from '../Heading/index';
 import { charityAPI } from '../../clients';
 import './styles.css';
+
+const CardLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={410}
+    viewBox="0 0 230 410"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <circle cx="117" cy="117" r="101" />
+    <rect x="51" y="246" rx="0" ry="0" width="150" height="10" />
+    <rect x="64" y="347" rx="0" ry="0" width="122" height="4" />
+    <rect x="85" y="316" rx="0" ry="0" width="83" height="4" />
+    <rect x="84" y="374" rx="0" ry="0" width="83" height="4" />
+  </ContentLoader>
+);
+
+const TitleLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={250}
+    height={35}
+    viewBox="0 0 250 50"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="0" ry="0" width="250" height="50" />
+  </ContentLoader>
+);
 
 export default class WorkStyle extends React.Component {
   state = {
@@ -21,14 +52,14 @@ export default class WorkStyle extends React.Component {
       .then(({ data }) => {
         this.setState({
           data: data,
-          loading: false,
+          loading: true,
           error: false
         });
       })
       .catch(error =>
         this.setState({
           error: true,
-          loading: false,
+          loading: true,
           errorMessage: " Couldn't fetch data"
         })
       );
@@ -36,7 +67,21 @@ export default class WorkStyle extends React.Component {
 
   render() {
     if (this.state.loading) {
-      return <div>Loading...</div>;
+      return (
+        <section className="work-style relative text-c600">
+          <div className="container">
+            <h2 className="flex justify-center my-8">
+              <TitleLoader  width = "100%" />
+            </h2>
+            <div className="flex flex-wrap justify-between  items-center  flex-col md:flex-row   ">
+              <CardLoader />
+              <CardLoader />
+              <CardLoader />
+              <CardLoader />
+            </div>
+          </div>
+        </section>
+      );
     }
 
     if (this.state.error) {
