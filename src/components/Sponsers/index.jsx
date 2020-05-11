ae13ff23eda26c98bd9c18ff43581cd97ba501fe
malngaawy <mohager477@gmail.com>
2020-05-11 14:11:42 +0200

    use react hooks

--- src/components/Sponsers/index.jsx
+++ src/components/Sponsers/index.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { charityAPI } from '../../clients';
 import './styles.css';
 
-class Sponsers extends React.Component {
-  state = { urls: [], error: false, loading: true };
-  componentDidMount() {
+const Sponsers = () => {
+  const [urls, setUrls] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
     charityAPI('/Sponsers')
       .then(res => {
-        this.setState({
-          urls: res.data,
-          error: false,
-          loading: false
-        });
+        setUrls(res.data);
+        setError(false);
+        setLoading(false);
       })
       .catch(error => {
-        this.setState({
-          error: true,
-          loading: false
-        });
+        setError(true);
+        setLoading(false);
       });
+  });
+  if (loading) {
+    return <div>loading data </div>;
   }
-  render() {
-    if (this.state.loading) {
-      return <div>loading data </div>;
-    }
-    if (this.state.error) {
-      return <div>we can not fetch data</div>;
-    }
-    return (
-      <section className="sponsors p-0 border-t sponser-border__top">
-        <div className="container">
-          <div className="mx-auto my-32 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
-            {this.state.urls.map(({ _id, image }) => (
-              <div className="sponser__logo my-2" key={_id}>
-                <img className="m-auto" src={image.url} alt="sponser" />
-              </div>
-            ))}
-          </div>
+  if (error) {
+    return <div>we can not fetch data</div>;
+  }
+  return (
+    <section className="sponsors p-0 border-t sponser-border__top">
+      <div className="container">
+        <div className="mx-auto my-32 grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+          {urls.map(({ _id, image }) => (
+            <div className="sponser__logo my-2" key={_id}>
+              <img className="m-auto" src={image.url} alt="sponser" />
+            </div>
+          ))}
         </div>
-      </section>
-    );
-  }
-}
+      </div>
+    </section>
+  );
+};
 export default Sponsers;
