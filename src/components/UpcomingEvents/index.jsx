658377d320734246b1663dc910adc35ed4aaafad
Tramsi <tramsi2014@gmail.com>
2020-05-12 21:56:05 +0200

    Removed  unused useRef

--- src/components/UpcomingEvents/index.jsx
+++ src/components/UpcomingEvents/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import { charityAPI } from '../../clients';
 import Heading from '../Heading';
