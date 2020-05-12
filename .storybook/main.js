c98f24ffb2d2eb8111a6607dd8c5d02473a868dd
user <great.nile@gmail.com>
2020-05-13 01:46:55 +0200

    integrate storybook with postcss, tailwind and purgecss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
+const { setupPostcss } = require('../config-webpack-postcss');
 module.exports = {
+  webpackFinal: config => {
+    return setupPostcss(config);
+  },
   stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
