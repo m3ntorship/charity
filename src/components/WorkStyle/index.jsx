0bec1819df04e154aa827160fbdb5fae3047b1d5
malngaawy <mohager477@gmail.com>
2020-05-12 11:28:49 +0200

    change some code places

--- src/components/WorkStyle/index.jsx
+++ src/components/WorkStyle/index.jsx
@@ -5,36 +5,6 @@ import Heading from '../Heading/index';
 import { charityAPI } from '../../clients';
 import './styles.css';
 
-const CardLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={230}
-    height={410}
-    viewBox="0 0 230 410"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
-    <circle cx="117" cy="117" r="101" />
-    <rect x="51" y="246" rx="0" ry="0" width="150" height="10" />
-    <rect x="64" y="347" rx="0" ry="0" width="122" height="4" />
-    <rect x="85" y="316" rx="0" ry="0" width="83" height="4" />
-    <rect x="84" y="374" rx="0" ry="0" width="83" height="4" />
-  </ContentLoader>
-);
-
-const TitleLoader = () => (
-  <ContentLoader
-    speed={2}
-    width={250}
-    height={35}
-    viewBox="0 0 250 50"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-  >
-    <rect x="0" y="0" rx="0" ry="0" width="250" height="50" />
-  </ContentLoader>
-);
-
 const WorkStyle = () => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
@@ -114,5 +84,35 @@ const WorkStyle = () => {
     </section>
   );
 };
+
+const CardLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={230}
+    height={410}
+    viewBox="0 0 230 410"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <circle cx="117" cy="117" r="101" />
+    <rect x="51" y="246" rx="0" ry="0" width="150" height="10" />
+    <rect x="64" y="347" rx="0" ry="0" width="122" height="4" />
+    <rect x="85" y="316" rx="0" ry="0" width="83" height="4" />
+    <rect x="84" y="374" rx="0" ry="0" width="83" height="4" />
+  </ContentLoader>
+);
+
+const TitleLoader = () => (
+  <ContentLoader
+    speed={2}
+    width={250}
+    height={35}
+    viewBox="0 0 250 50"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="0" ry="0" width="250" height="50" />
+  </ContentLoader>
+);
 
 export default WorkStyle;
