6835ef15537bdc830c13ea3afae124c4c3bd1861
Ahmad Samir <ahmedsamir96m@gmail.com>
2020-05-12 17:53:36 +0200

    made content loader responsive, adjusted it size

--- src/components/HeaderCarousel/ContentLoader/index.jsx
+++ src/components/HeaderCarousel/ContentLoader/index.jsx
@@ -3,7 +3,8 @@ import ContentLoader from 'react-content-loader';
 
 const Loader = props => (
   <ContentLoader
-    style={{ margin: 'auto' }}
+    style={{ margin: 'auto', display: 'flex' }}
+    preserveAspectRatio="none"
     speed={2}
     width={1024}
     height={600}
@@ -12,9 +13,9 @@ const Loader = props => (
     foregroundColor="#ecebeb"
   >
     <rect x="400" y="8" rx="3" ry="3" width="250" height="50" />
-    <rect x="142" y="91" rx="3" ry="3" width="750" height="140" />
-    <rect x="208" y="259" rx="3" ry="3" width="630" height="112" />
-    <rect x="413" y="402" rx="3" ry="3" width="225" height="72" />
+    <rect x="142" y="91" rx="3" ry="3" width="750" height="90" />
+    <rect x="212" y="208" rx="3" ry="3" width="630" height="81" />
+    <rect x="413" y="318" rx="3" ry="3" width="225" height="66" />
   </ContentLoader>
 );
 
