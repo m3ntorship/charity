80a7945ab4843cf43f0186c3a90b80c9a56132de
zahraa333 <zahraa.khalil2020@gmail.com>
2020-05-16 17:53:44 +0200

    added cards

--- src/components/Activities/index.jsx
+++ src/components/Activities/index.jsx
@@ -61,17 +61,28 @@ const Activities = () => {
               {data.description}
             </p>
           </div>
+      
 
           <div className="showcase-row -mt-3 px-8 grid gap-4 row-gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:gap-0 lg:row-gap-0">
-           <p></p>
+            {data.how_we_work_cards.map(card => (
+              <div className=" activity relative text-center" key={card._id}>
+                <img
+                  className="mx-auto"
+                  src={card.image_main.url}
+                  alt={card.image_main.alternativeText}
+                />
+                <h3 className="showcase-row__heading text-lg py-4">{card.Title}</h3>
+                <p className="text-base px-5">{card.description}</p>
+              </div>
+            ))}
           </div>
-
         </div>
         <div className="heart-bg w-1/2 h-48 -mt-48 p-0 xl:block hidden"></div>
       </Fragment>
     );
   }
 };
+
 
 
 
