4173f21e235124a56c83d790c68cd27af5ea552b
Tramsi <55321816+Tramsi@users.noreply.github.com>
2020-05-17 14:23:23 +0200

    Bugfix/multiple fixes (#285)
    * centering news and articles heading in mobile view
    
    * Enhancment to footer code
    
    * fixed footer triggeronce issue
    
    * Fixed uncaught promise bug and changed section to use usecharityapi
    
    * Fixed logo and top contact content loader issues
    
    * hide contact info loader in mobile view

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,61 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
-import { useSpring, animated } from 'react-spring';
+import React from 'react';
 import {
   AboutLoader,
   NewsletterLoader,
   ArticlesLoader,
   LinksLoader
 } from './FooterContentLoading';
-import { charityAPI } from '../../clients';
+import { useCharityAPI } from '../../clients';
 import Links from './Links/index';
 import Articles from './Articles/index';
 import About from './About/index';
 import Newsletter from './NewsLetter/index';
+import { useInView } from 'react-intersection-observer';
+import { useSpring, animated } from 'react-spring';
 
 const Footer = () => {
-  const [data, setData] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const getData = () => {
-    setLoading(true);
-    charityAPI('/footer')
-      .then(({ data }) => {
-        setData(data);
-        setLoading(false);
-        setError(false);
-      })
-      .catch(error => {
-        setLoading(false);
-        setError(true);
-        setErrorMessage("couldn't fetch data");
-      });
-  };
-
-  useEffect(() => {
-    setLoading(true);
-    getData();
-  }, []);
+  const { data, loading, dataError } = useCharityAPI('/footer');
 
   //Scroll observation
+
   const [refLeft, inViewLeft] = useInView({
-    threshold: 0.3,
     triggerOnce: true
   });
-  const [refRight, inViewRight] = useInView({
-    threshold: 0.3,
-    triggerOnce: true
-  });
-  const fadeRight = useSpring({
-    opacity: inViewRight ? 1 : 0,
-    transform: inViewRight ? 'translateX(0%)' : 'translateX(50%)'
-  });
+  const [refRight, inViewRight] = useInView({ triggerOnce: true });
+
   const fadeLeft = useSpring({
     opacity: inViewLeft ? 1 : 0,
     transform: inViewLeft ? 'translateX(0%)' : 'translateX(-50%)'
   });
+
+  const fadeRight = useSpring({
+    opacity: inViewRight ? 1 : 0,
+    transform: inViewRight ? 'translateX(0%)' : 'translateX(50%)'
+  });
+
+  if (dataError) {
+    return (
+      <div>
+        <p>{dataError}</p>
+      </div>
+    );
+  }
 
   if (loading) {
     return (
@@ -68,49 +52,43 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className="text-center py-8 text-sm border-t border-c700 bg-c100">
-            {data.Disclaimer}
-          </p>
+          <p className="text-center py-8 text-sm border-t border-c700 bg-c100"></p>
         </div>
       </footer>
     );
-  } else if (error) {
-    return (
-      <div>
-        {errorMessage},{' '}
-        <a href="#/" onClick={getData} className="text-c200">
-          retry?
-        </a>
-      </div>
-    );
-  } else {
+  }
+
+  if (data) {
     return (
       <footer className="footer bg-c100 text-c700">
         <div className="container w-9/12 grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 gap-4 py-16 text-sm font-hairline">
-          <animated.div
-            ref={refLeft}
-            style={fadeLeft}
-            className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1"
-          >
-            <About
-              title={data.About_title}
-              description={data.about_description}Right
-              url={data.about_button.url}
-              cta={data.about_button.text}
-            />
-            <Articles title={data.news_title} articles={data.articles} />
-          </animated.div>
-          <animated.div
-            ref={refRight}
-            style={fadeRight}
-            className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1"
-          >
-            <Links title="Links" links={data.links} />
-            <Newsletter
-              title={data.newsletter_title}
-              description={data.newsletter_description}
-            />
-          </animated.div>
+          <div className="ref-container" ref={refLeft}>
+            <animated.div
+              style={fadeLeft}
+              className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1"
+            >
+              <About
+                title={data.About_title}
+                description={data.about_description}
+                Right
+                url={data.about_button.url}
+                cta={data.about_button.text}
+              />
+              <Articles title={data.news_title} articles={data.articles} />
+            </animated.div>
+          </div>
+          <div className="ref-container" ref={refRight}>
+            <animated.div
+              style={fadeRight}
+              className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1"
+            >
+              <Links title="Links" links={data.links} />
+              <Newsletter
+                title={data.newsletter_title}
+                description={data.newsletter_description}
+              />
+            </animated.div>
+          </div>
         </div>
 
         <div>
@@ -121,6 +99,7 @@ const Footer = () => {
       </footer>
     );
   }
+  return 'generic error';
 };
 
 export default Footer;
