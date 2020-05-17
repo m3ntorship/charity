2f5fc6dfbe87db21361150f6223a36332e51ec3b
Tramsi <tramsi2014@gmail.com>
2020-05-17 16:53:08 +0200

    removed unused text

--- src/stories/5-Causes.stories.js
+++ src/stories/5-Causes.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { Causes } from '../components/Causes';
 
 export default {
