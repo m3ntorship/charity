2a1ef395b83b65cdbdefeb32e9b9a20c3c851f68
AbdoMagdy <abdoman46@hotmail.com>
2020-05-18 04:03:21 +0200

    made another story to showcase multiple slides

--- src/stories/9-HeaderCarousel.stories.js
+++ src/stories/9-HeaderCarousel.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { HeaderCarousel } from '../components/HeaderCarousel';
 
-const data = [
+const SingleSlideData = [
   {
     _id: '5eaed8f281864200173c4922',
     intro: 'Make an Impact',
@@ -23,6 +23,65 @@ const data = [
     }
   }
 ];
+const MultipleSlidesData = [
+  {
+    _id: '5eaed8f281864200173c4922',
+    intro: 'Leave Your Mark',
+    link: {
+      text: 'View Causes',
+      url: 'link url'
+    },
+    heading: {
+      heading_primary: 'Help Others',
+      heading_secondary: 'Make Thier Day'
+    },
+
+    image: {
+      name: 'children',
+
+      url:
+        'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+    }
+  },
+  {
+    _id: '5eaed8f281864200173c4922',
+    intro: 'Make an Impact',
+    link: {
+      text: 'View Causes',
+      url: 'link url'
+    },
+    heading: {
+      heading_primary: 'You Can Make',
+      heading_secondary: 'A Difference'
+    },
+
+    image: {
+      name: 'children',
+
+      url:
+        'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+    }
+  },
+  {
+    _id: '5eaed8f281864200173c4922',
+    intro: 'Make a Differenc',
+    link: {
+      text: 'View Causes',
+      url: 'link url'
+    },
+    heading: {
+      heading_primary: 'Spread Joy',
+      heading_secondary: 'in Other Hearts'
+    },
+
+    image: {
+      name: 'children',
+
+      url:
+        'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+    }
+  }
+];
 
 export default {
   title: 'HeaderCarousel',
@@ -30,10 +89,19 @@ export default {
   decorators: [withKnobs]
 };
 
-export const HeaderCarouselComponent = () => {
+export const HeaderCarouselSingleSlide = () => {
   return (
     <HeaderCarousel
-      data={data}
+      data={SingleSlideData}
+      loading={boolean('loading', false)}
+      error={boolean('error', false)}
+    />
+  );
+};
+export const HeaderCarouselMultipleSlides = () => {
+  return (
+    <HeaderCarousel
+      data={MultipleSlidesData}
       loading={boolean('loading', false)}
       error={boolean('error', false)}
     />
