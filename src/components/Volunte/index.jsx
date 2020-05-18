6e90bb5cdbcd2166e0dd2f83da5158825f64c119
malngaawy <mohager477@gmail.com>
2020-05-18 21:08:13 +0200

    add animation

--- src/components/Volunte/index.jsx
+++ src/components/Volunte/index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ContentLoader from 'react-content-loader';
+import { useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
 import bg from './img/blocks.png';
 import './styles.css';
 
 export const Volunte = ({ data, loading, error }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 1
+  });
+  const fade1 = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? 'translateX(0%)' : 'translateX(50%)'
+  });
+  const fade2 = useSpring({
+    opacity: inView ? 1 : 0,
+    transform: inView ? 'translateX(0%)' : 'translateX(-50%)'
+  });
+
   if (error) {
     return (
       <div className="volunte bg-c200 text-center text-xl text-c000 py-12 mt-5">
@@ -32,20 +47,29 @@ export const Volunte = ({ data, loading, error }) => {
     button: { title, link }
   } = data;
   return (
-    <div className="volunte bg-c200 py-12 mt-5 relative overflow-hidden">
+    <div
+      ref={ref}
+      className="volunte bg-c200 py-12 mt-5 relative overflow-hidden"
+    >
       <div className="container">
         <img src={bg} className="absolute" />
-        <div className="description w-full text-center md:text-left md:w-5/12 inline-block text-c000 mb-10 md:mb-auto md:ml-20">
+        <animated.div
+          style={fade2}
+          className="description w-full text-center md:text-left md:w-5/12 inline-block text-c000 mb-10 md:mb-auto md:ml-20"
+        >
           <p className="font-bold">{description}</p>
-        </div>
-        <div className="inline-block w-full md:w-5/12 text-center">
+        </animated.div>
+        <animated.div
+          style={fade1}
+          className="inline-block w-full md:w-5/12 text-center"
+        >
           <button className="btn btn-sm md:float-right px-10 text-c100 text-sm font-bold bg-c300">
             {' '}
             <a href={link} className="text-lg">
               {title}
             </a>{' '}
           </button>
-        </div>
+        </animated.div>
       </div>
     </div>
   );
