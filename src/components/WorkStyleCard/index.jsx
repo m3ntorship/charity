96910be5798ec4ff6945059d65155755b25b963b
AbdoMagdy <abdoman46@hotmail.com>
2020-05-18 05:03:55 +0200

    convert to a function based component

--- src/components/WorkStyleCard/index.jsx
+++ src/components/WorkStyleCard/index.jsx
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
 import { Transition } from 'react-spring/renderprops';
 import { animated } from 'react-spring';
 
-export default class WorkStyleCard extends React.Component {
-  constructor() {
-    super();
-    this.state = { isHovered: false };
+const  WorkStyleCard = (description,
+              title,
+              img,
+              img_hover,
+              border_color,
+              key,
+              fade) => {
+
+    const[isHovered, setHoverState] = useState(false)
+  
+  const borderColor = {
+    red: 'border-c200',
+    green: 'border-c400',
+    yellow: 'border-c300',
+    blue: 'border-c500'
   }
-  render() {
-    const borderColor = {
-      red: 'border-c200',
-      green: 'border-c400',
-      yellow: 'border-c300',
-      blue: 'border-c500'
-    };
     return (
       <animated.div
         className=" card__wrapper text-center flex flex-col col-div items-center my-8   md:my-auto"
-        onMouseEnter={() => this.setState({ isHovered: true })}
-        onMouseLeave={() => this.setState({ isHovered: false })}
-        style={this.props.fade}
+        onMouseEnter={setHoverState(true)}
+        onMouseLeave={setHoverState(false)}
+        style={fade}
       >
         <div
           className={cn(
             'icon-div  h-48 w-48 border-solid  border-4 md:border-6 lg:border-8 rounded-full flex justify-center p-2 items-center relative cursor-pointer',
-            borderColor[this.props.border_color]
+            borderColor[border_color]
           )}
         >
           <Transition
             config={{ duration: 400 }}
-            items={this.state.isHovered}
+            items={isHovered}
             from={{ position: 'absolute', opacity: 0 }}
             enter={{ opacity: 1 }}
             leave={{ opacity: 0 }}
@@ -44,22 +48,22 @@ export default class WorkStyleCard extends React.Component {
                     >
                       <img
                         className="rounded-full h-full w-full"
-                        src={this.props.img_hover}
-                        alt={this.props.title}
+                        src={img_hover}
+                        alt={title}
                       />
                     </div>
                   )
                 : props => (
                     <div className="w-5/12" style={props}>
-                      <img src={this.props.img} alt={this.props.title} />
+                      <img src={img} alt={title} />
                     </div>
                   )
             }
           </Transition>
         </div>
-        <h3 className="showcase-row__heading my-2">{this.props.title}</h3>
+        <h3 className="showcase-row__heading my-2">{title}</h3>
         <p className="showcase-row__description text-base py-2">
-          {this.props.description}
+          {description}
         </p>
       </animated.div>
     );
