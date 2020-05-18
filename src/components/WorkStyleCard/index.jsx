973ec4d5411f866be2883c11c45d5681bd707161
AbdoMagdy <abdoman46@hotmail.com>
2020-05-18 05:13:12 +0200

    used Hooks for animation

--- src/components/WorkStyleCard/index.jsx
+++ src/components/WorkStyleCard/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import cn from 'classnames';
-import { Transition } from 'react-spring/renderprops';
-import { animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
 
 const WorkStyleCard = (
   description,
@@ -9,11 +8,15 @@ const WorkStyleCard = (
   img,
   img_hover,
   border_color,
-  key,
   fade
 ) => {
   const [isHovered, setHoverState] = useState(false);
-
+  const transitions = useTransition(isHovered, null, {
+    from: { position: 'absolute', opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    config: { duration: 400 }
+  });
   const borderColor = {
     red: 'border-c200',
     green: 'border-c400',
@@ -33,31 +36,24 @@ const WorkStyleCard = (
           borderColor[border_color]
         )}
       >
-        <Transition
-          config={{ duration: 400 }}
-          items={isHovered}
-          from={{ position: 'absolute', opacity: 0 }}
-          enter={{ opacity: 1 }}
-          leave={{ opacity: 0 }}
-        >
-          {toggle =>
-            toggle
-              ? props => (
-                  <div className="h-full w-full p-2 rounded-full" style={props}>
-                    <img
-                      className="rounded-full h-full w-full"
-                      src={img_hover}
-                      alt={title}
-                    />
-                  </div>
-                )
-              : props => (
-                  <div className="w-5/12" style={props}>
-                    <img src={img} alt={title} />
-                  </div>
-                )
-          }
-        </Transition>
+        {transitions.map(({ item, key, props }) =>
+          item ? (
+            <animated.div
+              className="h-full w-full p-2 rounded-full"
+              style={props}
+            >
+              <img
+                className="rounded-full h-full w-full"
+                src={img_hover}
+                alt={title}
+              />
+            </animated.div>
+          ) : (
+            <animated.div className="w-5/12" style={props}>
+              <img src={img} alt={title} />
+            </animated.div>
+          )
+        )}
       </div>
       <h3 className="showcase-row__heading my-2">{title}</h3>
       <p className="showcase-row__description text-base py-2">{description}</p>
