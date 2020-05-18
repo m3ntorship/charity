79e1b860127c349dd6ab4af95810cd4a19ade4ce
malngaawy <mohager477@gmail.com>
2020-05-18 02:53:57 +0200

    separate to two component

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -40,84 +40,135 @@ export const ContactInfoContainer = () => {
 };
 
 export const ContactInfo = ({
-  contactData,
   socialData,
-  error,
-  errorMessage,
-  loading
+  contactData,
+  socialError,
+  contactError,
+  socialErrorMessage,
+  contactErrorMessage,
+  socialLoading,
+  contactLoading
 }) => {
-  if (error) {
-    return (
-      <div>{`${errorMessage}: Error while fetching socialmedia or contacts`}</div>
-    );
-  }
-
-  if (loading) {
-    return 'Loading ...';
-  }
-
   return (
     <section className="contact-section py-10">
       <div className="container">
         <div className="flex flex-col md:flex-row justify-around items-center mx-0 text-c000">
-          <div className="contact-section__social flex justify-start text-xxs mb-6 md:mb-0">
-            {socialData.map(({ _id, url, fontawesome_icons }) => (
-              <div
-                key={_id}
-                className="contact-section__social-icon hover:bg-c200 ml-0"
-              >
+          <Socialmedia
+            socialData={socialData}
+            socialLoading={socialLoading}
+            socialError={socialError}
+            socialErrorMessage={socialErrorMessage}
+          />
+          <Contact
+            contactData={contactData}
+            contactLoading={contactLoading}
+            contactError={contactError}
+            contactErrorMessage={contactErrorMessage}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Socialmedia = ({
+  socialData,
+  socialLoading,
+  socialError,
+  socialErrorMessage
+}) => {
+  if (socialError) {
+    return (
+      <div className="bg-c200 py-5 text-c000">
+        <h2>Sorry we have got an error : {socialErrorMessage}</h2>
+      </div>
+    );
+  }
+
+  if (socialLoading) {
+    <div>Loading....</div>;
+  }
+
+  return (
+    <div className="contact-section__social flex justify-start text-xxs mb-6 md:mb-0">
+      {socialData.map(({ _id, url, fontawesome_icons }) => (
+        <div
+          key={_id}
+          className="contact-section__social-icon hover:bg-c200 ml-0"
+        >
+          <a
+            className="full-width-click"
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className={fontawesome_icons}></i>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Contact = ({
+  contactData,
+  contactLoading,
+  contactError,
+  contactErrorMessage
+}) => {
+  if (contactError) {
+    <div className="bg-c200 py-5 text-c000">
+      <h2>Sorry we have got an error : {contactErrorMessage}</h2>
+    </div>;
+  }
+
+  if (contactLoading) {
+    <div>Loading...</div>;
+  }
+
+  return (
+    <div className="contact-section__contacts flex flex-no-wrap flex-col lg:flex-row justify-end text-sm mx-6">
+      {contactData.map(
+        (
+          { _id, title, url, sub_title, icon: { url: iconUrl, name } },
+          index
+        ) => {
+          const isLast = index === contactData.length - 1;
+          return (
+            <div
+              key={_id}
+              className={cn(
+                'contact',
+                'my-1',
+                'lg:my-0',
+                'w-64',
+                'mt-2',
+                'md:mt-0',
+                {
+                  'border-right': !isLast
+                }
+              )}
+            >
+              <div className="contact-icon">
+                <img className="h-auto" src={iconUrl} alt={name} />
+              </div>
+              <div className="information">
                 <a
-                  className="full-width-click"
+                  className="block"
                   href={url}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <i className={fontawesome_icons}></i>
+                  {title}
                 </a>
+                <small className="information-small leading-normal">
+                  {sub_title}
+                </small>
               </div>
-            ))}
-          </div>
-
-          <div className="contact-section__contacts flex flex-no-wrap flex-col lg:flex-row justify-end text-sm mx-6">
-            {contactData.map(({ _id, title, url, sub_title, icon }, index) => {
-              const isLast = index === contactData.length - 1;
-              return (
-                <div
-                  key={_id}
-                  className={cn(
-                    'contact',
-                    'my-1',
-                    'lg:my-0',
-                    'w-64',
-                    'mt-2',
-                    'md:mt-0',
-                    {
-                      'border-right': !isLast
-                    }
-                  )}
-                >
-                  <div className="contact-icon">
-                    <img className="h-auto" src={icon.url} alt={icon.name} />
-                  </div>
-                  <div className="information">
-                    <a
-                      className="block"
-                      href={url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {title}
-                    </a>
-                    <small className="information-small leading-normal">
-                      {sub_title}
-                    </small>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </section>
+            </div>
+          );
+        }
+      )}
+    </div>
   );
 };
