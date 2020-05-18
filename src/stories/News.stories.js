d5732e8105e7d07a31e935164530f7e035b195ed
AbdoMagdy <abdoman46@hotmail.com>
2020-05-18 15:21:58 +0200

    removed unnecessary import

--- src/stories/News.stories.js
+++ src/stories/News.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { News } from '../components/NewsAndArticles';
 
 export default {
@@ -18,13 +18,13 @@ const data = {
       link: {
         id: '5eaa1b013482057530be1bf0',
         text: 'Read more',
-        url: '#readmore',
+        url: '#readmore'
       },
       image: {
         url:
           'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/causes-card3_a56e0bb05d_62e507b91e.jpeg',
         id: '5eb899a60e79a21caf96f6e0'
-      },
+      }
     },
     {
       id: '5eaaf887d628f6001779b7c6',
@@ -33,15 +33,15 @@ const data = {
       link: {
         id: '5eaaf887d628f6001779b7c7',
         text: 'Read More',
-        url: '#readmore2',
+        url: '#readmore2'
       },
       image: {
         id: '5eb89af10e79a21caf96f702',
         name: 'causes-card2_8563b353ef_b5daf5114b',
         url:
-          'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/causes-card2_8563b353ef_b5daf5114b_ace5db76e1.jpeg',
+          'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/causes-card2_8563b353ef_b5daf5114b_ace5db76e1.jpeg'
       },
-      date: '2020-05-12T10:00:00.000Z',
+      date: '2020-05-12T10:00:00.000Z'
     },
     {
       id: '5eaaf8c8d628f6001779b7cb',
@@ -50,15 +50,15 @@ const data = {
       link: {
         id: '5eaaf8c8d628f6001779b7cc',
         text: 'Read More',
-        url: '#readmre3',
+        url: '#readmre3'
       },
       image: {
         id: '5eb89b9d0e79a21caf96f717',
-        name: 'causes-card1_eb5234d0f2',    
+        name: 'causes-card1_eb5234d0f2',
         url:
-          'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/causes-card1_eb5234d0f2_a67e8efb99.jpeg',
+          'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/causes-card1_eb5234d0f2_a67e8efb99.jpeg'
       },
-      date: '2020-05-07T10:00:00.000Z',
+      date: '2020-05-07T10:00:00.000Z'
     }
   ],
   id: '5eaa1b3e3482057530be1bf3',
@@ -67,13 +67,13 @@ const data = {
   heading: {
     id: '5eaa1b3e3482057530be1bf4',
     heading_primary: 'News &',
-    heading_secondary: 'Articles',
+    heading_secondary: 'Articles'
   },
   link: {
     id: '5eaa1b3e3482057530be1bf6',
     url: '#all',
-    text: 'View All Posts',
-  },
+    text: 'View All Posts'
+  }
 };
 
 export const NewsComponent = () => {
@@ -82,7 +82,6 @@ export const NewsComponent = () => {
       data={data}
       loading={boolean('loading', false)}
       error={boolean('error', false)}
-
     />
   );
 };
