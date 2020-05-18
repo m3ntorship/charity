f1cf4f5bc3032dfea17f5035ce982dbbc8b6f587
zahraa333 <zahraa.khalil2020@gmail.com>
2020-05-18 02:51:38 +0200

    added story file

---
+++ src/stories/Logo.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { Logo } from '../components/Logo';
+
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  decorators: [withKnobs]
+};
+
+const data = {
+  logo: {
+    url:
+      'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/logo_eba40dfa8c.png',
+    id: '5ea50b81186dd70017900752'
+  },
+  id: '5ea50b84186dd70017900753'
+};
+export const LogoComponent = () => {
+  return (
+    <WorkStyle
+      data={data}
+      loading={boolean('loading', false)}
+      error={boolean('error', false)}
+      errorMessage={text('errorMessage', '')}
+    />
+  );
+};
