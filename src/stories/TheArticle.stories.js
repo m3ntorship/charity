d3b5a9b4635e19a21f6d9de17a65bd2f71ebb8da
zahraa333 <zahraa.khalil2020@gmail.com>
2020-05-19 07:33:17 +0200

    delete unused code

--- src/stories/TheArticle.stories.js
+++ src/stories/TheArticle.stories.js
@@ -1,32 +1,16 @@
-// import React from 'react';
-// import { withKnobs, boolean, text } from '@storybook/addon-knobs';
-
-
-// export default {
-//   title: 'Article',
-//   component: Article,
-//   decorators: [withKnobs]
-// };
-
-
-// export const Article = () => {
-//   return (
-//    <p>hello articles</p>
-//   );
-// };
-
-
 import React from 'react';
-import { Button } from '@storybook/react/demo';
+import { withKnobs } from '@storybook/addon-knobs';
 
-export default { title: 'Button' };
 
-export const withText = () => <Button>Hello Button</Button>;
+export default {
+  title: 'Article',
+  component: Article,
+  decorators: [withKnobs]
+};
 
-export const withEmoji = () => (
-  <Button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+
+export const Article = () => {
+  return (
+  
+  );
+};
