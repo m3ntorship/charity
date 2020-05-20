13af4ff1ae39811238e1a471499835d2c8e6fcdf
zahraa333 <zahraa.khalil2020@gmail.com>
2020-05-20 02:16:01 +0200

    linked dump components to smart component then to storybook file

--- src/components/ArticleDetails/index.js
+++ src/components/ArticleDetails/index.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import './styles.css';
 
-
 const border = {
   borderColor: '#f5f5f5'
 };
 
 
 
-const ArticleContainer = () => {
-  return (
-    <div className="grid grid-cols-12 gap-4 pt-16 p-3">
-      <ArticleImg />
+const ArticleContainer = ({ data, loading, error }) => {
+  if (error) {
+    return 'error';
+  } else if (loading) {
+    return 'loading';
+  } else if (data) {
+    const { headline, img, body_text, conclusion_text } = data;
+    return (
+      <div className="grid grid-cols-12 gap-4 pt-16 p-3">
+        <ArticleImg img_main={img.primary.url} />
 
-      <Headline />
+        <Headline head={headline} />
 
-      <ArticleBody />
+        <ArticleBody body={body_text} />
 
-      <BodyImg />
+        <BodyImg img_secondary={img.secondary.url} />
 
-      <ArticleBody />
-    </div>
-  );
+        <ArticleBody body={conclusion_text} />
+      </div>
+    );
+  }
 };
 
 // article headline
-const Headline = () => {
+const Headline = ({head}) => {
   return (
     <header
       className="col-start-1 col-end-13 border-b-2 border-red-600
@@ -37,12 +43,12 @@ const Headline = () => {
       <div className="content-info text-center md:text-left">
         <span className="text-c600 mr-2 text-xxs">
           <i className="fas fa-user-tie mr-1 text-c500"></i>Admin
-        </span>
+        </span> 
         <span className="text-c600 mr-2 text-xxs">
           <i className="fas fa-comments mr-1 text-c500"></i>2 Comments
         </span>
         <h4 className="text-c100 font-bold pb-8 text-large">
-          Water Need in Africa
+          {head} 
         </h4>
       </div>
     </header>
@@ -50,7 +56,7 @@ const Headline = () => {
 };
 
 // article body
-const ArticleBody = () => {
+const ArticleBody = ({body}) => {
   return (
     <article
       className="main_article col-start-1 text-justify col-end-13  text-c600 text-sm
@@ -59,18 +65,14 @@ const ArticleBody = () => {
         lg:col-start-3 lg:col-end-8"
     >
       <p>
-        venenatis et lectus. Vivamus justo arcu, varius in felis eleifend,
-        imperdiet condimentum massa. In ipsum orci, facilisis vitae lacinia a,
-        aliquam ac nisl. Nulla facilisi. Nullam ultrices tellus vel nisl
-        tristique, id fringilla nunc vulputate. Sed efficitur mauris eu dolor
-        semper, non porttitor nisl fermentum.
+       {body}
       </p>
     </article>
   );
 };
 
 // article main img
-const ArticleImg = () => {
+const ArticleImg = ({img_main}) => {
   return (
     <div
       className="bg-c900 w-full h-64 col-start-1 col-end-13
@@ -78,25 +80,30 @@ const ArticleImg = () => {
         md:col-start-1 md:col-end-8 md:bg-c600
         lg:col-start-3 lg:col-end-8 lg:bg-c400"
     >
-      <div className="articleImg"></div>
+      <img src={img_main} alt="" />
     </div>
   );
 };
 
 // article body img
 
-const BodyImg = () => {
+const BodyImg = ({img_secondary}) => {
   return (
     <div
-      className="col-start-1 col-end-13
+      className="flex col-start-1 col-end-13
         sm:col-start-2 sm:col-end-12
         md:col-start-1 md:col-end-8
         lg:col-start-3 lg:col-end-8"
     >
-      <div className=" bg-c100 ">image 1</div>
-      <div className=" bg-c200  ">image 2</div>
+      <div className="w-40 h-40 bg-c100 mr-8">
+        <img src={img_secondary} alt="" />
+      </div>
+      <div className="w-40 h-40 bg-c200  ">
+        <img src={img_secondary} alt="" />
+      </div>
     </div>
   );
 };
 
 export default ArticleContainer;
+
