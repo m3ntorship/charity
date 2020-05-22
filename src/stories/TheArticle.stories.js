2d2f84cb0db3402f19707e9d3d674fffc66c6770
Zahraa-Khalil <zahraa.khalil2020@gmail.com>
2020-05-22 21:30:20 +0200

    Feature/article page (#320)
    * added article component + styles
    
    * created story file
    
    * delete unused code
    
    * linked dump components to smart component then to storybook file
    
    * linked smart component
    
    * modified components + linked smart component to storybook
    
    * Linked story to component + added data
    
    * add loader
    
    * added loading components + updated user & admin to recieve data from api
    
    * modified data with user and comments

---
+++ src/stories/TheArticle.stories.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+import ArticleModel from '../components/ArticleDetails';
+
+export default {
+  title: 'Article',
+  component: ArticleModel,
+  decorators: [withKnobs]
+};
+
+const data = {
+  id: '1',
+  headline: 'Water Need in Africa',
+  user_type:'Admin',
+  comments:['lorem','ipsum'],
+  body_text: [
+    {
+      id: '12',
+      text:
+        'venenatis et lectus. Vivamus justo arcu, varius in felis eleifend,imperdiet condimentum massa. In ipsum orci, facilisis vitae lacinia a,aliquam ac nisl. Nulla facilisi. Nullam ultrices tellus vel nisltristique, id fringilla nunc vulputate. Sed efficitur mauris eu dolorsemper, non porttitor nisl fermentum.'
+    },
+    {
+      id: '123',
+      text:
+        'venenatis et lectus. Vivamus justo arcu, varius in felis eleifend,imperdiet condimentum massa. In ipsum orci, facilisis vitae lacinia a,aliquam ac nisl. Nulla facilisi. Nullam ultrices tellus vel nisltristique, id fringilla nunc vulputate. Sed efficitur mauris eu dolorsemper, non porttitor nisl fermentum.'
+    },
+    {
+      id: '1234',
+      text:
+        'venenatis et lectus. Vivamus justo arcu, varius in felis eleifend,imperdiet condimentum massa. In ipsum orci, facilisis vitae lacinia a,aliquam ac nisl. Nulla facilisi. Nullam ultrices tellus vel nisltristique, id fringilla nunc vulputate. Sed efficitur mauris eu dolorsemper, non porttitor nisl fermentum.'
+    }
+  ],
+
+  img_primary: {
+    url:
+      'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+  },
+  img_secondary: [
+    {
+      id: '1',
+      url:
+        'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+    },
+    {
+      id: '2',
+      url:
+        'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
+    }
+  ],
+  conclusion_text:
+    'Lorem ipsum dolor sit amet, consectetur notted adipisi cing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua do eiusmod tempor incididunt ut labore et dolore magna aliqua do eiusmod.'
+};
+  
+
+
+export const ArticlePage = () => {
+  return (
+    <ArticleModel
+      data={data}
+      loading={boolean('loading', false)}
+      error={boolean('error', false)}
+    />
+  );
+};
+
+
+
