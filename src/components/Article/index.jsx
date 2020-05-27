312d3e04d696492aa7d76478dc8ac38929221cd8
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2020-05-27 19:14:13 +0400

    Feature/add article storybook (#351)
    * moved css style to Article compomnet
    
    * imported css style file
    
    * added style for article andr renamed css selectors  names
    
    * added single article story
    
    * removed unsed css style

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css'
 import { useInView } from 'react-intersection-observer';
 import { useSpring, animated } from 'react-spring';
 import useMedia from '../../Helpers/useMedia';
