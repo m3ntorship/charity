2dc81a2ac95aa36ad46f24d8d9ff6d94600f7a1b
malngaawy <mohager477@gmail.com>
2020-05-27 15:22:50 +0200

    make contact top button true

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import { LogoContainer } from '../Logo';
 import { MainNavigationContainer } from '../MainNavigation';
 const Header = () => (
   <header>
-    <ContactTopContainer haveBtn={false} />
+    <ContactTopContainer haveBtn={true} />
     <section className="logo-section py-8 hidden md:block">
       <div className="container py-2 flex items-center justify-between">
         <LogoContainer />
