04fe7154b5bf3a6abf6187bd391ad12fabed9eee
malngaawy <mohager477@gmail.com>
2020-05-27 17:55:46 +0200

    edit the new paths of imported files

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { WorkStyleContainer } from './components/WorkStyle';
-import { FeaturedBannerContainer } from './components/FeaturedBanner';
-import { NumbersContainer } from './components/Numbers';
-import { CausesContainer } from './components/Causes';
-import { SponsersContainer } from './components/Sponsers';
-import { ActivitiesContainer } from './components/Activities';
-import Welcome from './components/Welcome';
-import { ContactInfoContainer } from './components/ContactInfo';
-import { TestimonialsContainer } from './components/Testimonials';
-import { UpcomingEventsSectionContainer } from './components/UpcomingEvents';
-import { NewsConatiner } from './components/NewsAndArticles';
-import { HeaderCarouselContainer } from './components/HeaderCarousel';
+import { WorkStyleContainer } from '../../components/WorkStyle';
+import { FeaturedBannerContainer } from '../../components/FeaturedBanner';
+import { NumbersContainer } from '../../components/Numbers';
+import { CausesContainer } from '../../components/Causes';
+import { SponsersContainer } from '../../components/Sponsers';
+import { ActivitiesContainer } from '../../components/Activities';
+import Welcome from '../../components/Welcome';
+import { ContactInfoContainer } from '../../components/ContactInfo';
+import { TestimonialsContainer } from '../../components/Testimonials';
+import { UpcomingEventsSectionContainer } from '../../components/UpcomingEvents';
+import { NewsConatiner } from '../../components/NewsAndArticles';
+import { HeaderCarouselContainer } from '../../components/HeaderCarousel';
 
 const HomeContainer = () => {
   return <Home />;
