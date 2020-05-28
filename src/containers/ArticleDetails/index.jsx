ec5a14733108542b037d147492c9518b9635c923
malngaawy <mohager477@gmail.com>
2020-05-28 16:40:38 +0200

    remove banner data object .. won't used any more

--- src/containers/ArticleDetails/index.jsx
+++ src/containers/ArticleDetails/index.jsx
@@ -5,28 +5,6 @@ import { Banner } from '../../components/ArticleBanner';
 import { ArticlesSearch } from '../../components/ArticlesSearch';
 import { RecentArticlesContainer } from '../../components/RecentArticles';
 import { useCharityAPI } from '../../clients/index';
-const bannerData = {
-  links: [
-    { text: 'Home', url: '#home', id: '1' },
-    { text: 'latest news', url: '#latest news', id: '2' }
-  ],
-  _id: '5eaed8f281864200173c4922',
-  intro: 'Make an Impact',
-  link: {
-    text: 'View Causes',
-    url: 'link url'
-  },
-  heading: {
-    heading_primary: 'News & Articles'
-  },
-
-  image: {
-    name: 'children',
-
-    url:
-      'https://charity-cms-dev.s3.eu-central-1.amazonaws.com/children_5a459dcc62.jpeg'
-  }
-};
 const volunterringData = {
   description: 'Best Way ot Make a Difference in the Lives of Others',
   button: {
