10e1ce709ee8ec91ab8342944560c9c6556f6a98
malngaawy <mohager477@gmail.com>
2020-05-29 17:36:02 +0200

    add loading and error props to articleModel com

--- src/components/ArticlePageContent/index.jsx
+++ src/components/ArticlePageContent/index.jsx
@@ -26,7 +26,11 @@ export const ArticlePageContent = () => {
     return (
       <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-8 row-gap-8">
         <div className="col-span-12 lg:col-span-8">
-          <ArticleModel data={articleData} />
+          <ArticleModel
+            data={articleData}
+            loading={articleLoading}
+            error={error}
+          />
         </div>
         <aside className="col-span-12 lg:col-span-4 flex flex-col sm:flex-row lg:flex-col">
           <div className="mb-8 sm:mr-8 lg:mr-0 sm:w-1/2 lg:w-full">
