44f5cfa96c1c0eb981deb17a159f403c1248a4e8
elhaw <abd.elsayed@hotmail.com>
2020-06-07 00:40:10 +0400

    get data for upcoming events form redux store

--- src/components/UpcomingEvents/index.jsx
+++ src/components/UpcomingEvents/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useSelector} from 'react-redux'
 import './styles.css';
 import { useCharityAPI } from '../../clients';
 import Heading from '../Heading';
@@ -9,7 +10,8 @@ import { useInView } from 'react-intersection-observer';
 import { useSpring, animated } from 'react-spring';
 
 const UpcomingEventsSectionContainer = () => {
-  const { data, loading, dataError: error } = useCharityAPI('/upcoming-events');
+  const { data, loading, error } = useSelector(store => store.upcomingEvents);
+
   const {
     data: cardData,
     loading: cardLoading,
