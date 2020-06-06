3c3080f91e8f6013d9c3728609deab710c8ab408
malngaawy <mohager477@gmail.com>
2020-06-07 00:15:14 +0200

    create contact reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 import { numbersReducer } from './reducers/numbers';
 import { testimonialsReducer } from './reducers/testimonials';
+import { contactReducer } from './reducers/contact';
+import { socialReducer } from './reducers/social';
 
 const CharityApp = combineReducers({
   testimonials: testimonialsReducer,
-  numbers: numbersReducer
+  numbers: numbersReducer,
+  social: socialReducer,
+  contact: contactReducer
 });
 
 const store = createStore(
