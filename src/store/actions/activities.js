cf2f69659463a4102e16cd195b3f336d76c38f3d
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-06 19:28:53 +0200

    add action to activities component

---
+++ src/store/actions/activities.js
@@ -0,0 +1,22 @@
+import { ACTION_TYPES } from '../actionTypes';
+
+export const setActivitiesData = data => {
+  return {
+    type: ACTION_TYPES.ACTIVITIES.SET_DATA,
+    paload: data
+  };
+}
+
+export const setActivitiesLoading = loading => {
+         return {
+           type: ACTION_TYPES.ACTIVITIES.SET_LOADING,
+           payload: loading
+         };
+       };
+
+export const setActivitiesError = errorState => {
+         return {
+           type: ACTION_TYPES.ACTIVITIES.SET_ERROR,
+           payload: errorState
+         };
+       };
