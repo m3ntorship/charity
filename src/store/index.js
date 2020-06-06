69668f7f4e95bd7b515e6c02588effb922161c8a
AbdoMagdy <abdoman46@hotmail.com>
2020-06-06 06:47:46 +0200

    implement redux store

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from 'redux';
+import { activitesReducer } from './reducers';
+const initialState = {
+  activites: {}
+};
+
+const topReducer = combineReducers({
+  activites: activitesReducer
+});
+
+const store = createStore(
+  topReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
