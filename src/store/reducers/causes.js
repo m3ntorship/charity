210ee49cfde6391a7d5c8b6189efea2e5cf2beae
elhaw <abd.elsayed@hotmail.com>
2020-06-08 03:00:22 +0400

    added sinitaztion logic in the action

--- src/store/reducers/causes.js
+++ src/store/reducers/causes.js
@@ -5,45 +5,12 @@ const initialState = {
   loading: false,
   error: false
 };
-const dataSanitization = data => {
-  if (data) {
-    const {
-      id,
-      causes,
-      causes_heading: { heading_primary, heading_secondary }
-    } = data;
-    const sanitizedCauses = causes.map(
-      ({ title, description, raised, goal, image: { url: imgURL }, id }) => {
-        return {
-          title,
-          description,
-          raised,
-          goal,
-          imgURL,
-          id
-        };
-      }
-    );
-    return {
-      id,
-      causes: sanitizedCauses,
-      causes_heading: {
-        heading_primary,
-        heading_secondary
-      }
-    };
-  }
-  else {
-    return null
-  }
-};
+
 
 export const causesReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ACTION_TYPES.CAUSES.SET_DATA:
-      let sanitizeData = dataSanitization(payload);
-      console.log(`value of sanitized data is ${sanitizeData}`)
-      const newState = { ...state, data: sanitizeData };
+      const newState = { ...state, data: payload };
       return newState;
     case ACTION_TYPES.CAUSES.SET_LOADING:
       return { ...state, loading: payload };
