150b4ac010f4c43df598265d745b372cebeb6ccc
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-07 18:44:49 +0200

    modify logo directory

--- src/components/MainNavigation/index.jsx
+++ src/components/MainNavigation/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import useMedia from '../../Helpers/useMedia';
 import { useCharityAPI } from '../../clients';
 import NavigationLink from '../NavigationLink';
-import { LogoContainer } from '../Logo';
+import { LogoContainer } from '../../containers/layout/logo';
 import Loader from './ContentLoader';
 import './style.css';
 
