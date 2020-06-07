9d01a4e400ec99160ae07149d66c195da050b26a
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-07 22:22:16 +0200

    fix typo errors

--- src/containers/layout/logo.js
+++ src/containers/layout/logo.js
@@ -5,7 +5,7 @@ import { Logo } from '../../components/Logo';
 
 
 export const LogoContainer = () => {
-  const { data, loading, dataError } = useSelector(store => store.footer);
+  const { data, loading, dataError } = useSelector(store => store.logo);
   return <Logo data={data} loading={loading} error={dataError} />;
 };
 
