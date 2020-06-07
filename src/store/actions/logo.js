15dae230b1e9b658baf6650c529642bb1f090891
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-07 18:50:07 +0200

    link logo to app.js

--- src/store/actions/logo.js
+++ src/store/actions/logo.js
@@ -1,20 +1,20 @@
 import { ACTION_TYPES } from '../actionTypes';
 
-export const setFooterData = data => {
+export const setLogoData = data => {
   return {
     type: ACTION_TYPES.LOGO.SET_DATA,
     payload: data
   };
 };
 
-export const setFooterLoading = loading => {
+export const setLogoLoading = loading => {
   return {
     type: ACTION_TYPES.LOGO.SET_LOADING,
     payload: loading
   };
 };
 
-export const setFooterError = errorState => {
+export const setLogoError = errorState => {
   return {
     type: ACTION_TYPES.LOGO.SET_ERROR,
     payload: errorState
