709a8943b4871ef0e59bc52054416c7d1656de93
malngaawy <mohager477@gmail.com>
2020-06-08 01:02:49 +0200

    remove un used console.log

--- src/store/reducers/contact.js
+++ src/store/reducers/contact.js
@@ -10,7 +10,6 @@ export const contactReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ACTION_TYPES.CONTACTINFO.SET_DATA:
       const newState = { ...state, data: payload };
-      console.log(newState.data);
       return newState;
     case ACTION_TYPES.CONTACTINFO.SET_LOADING:
       return { ...state, loading: payload };
