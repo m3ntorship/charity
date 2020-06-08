bf560edeb9d2eea48536e3206d0be17660af5b26
elhaw <abd.elsayed@hotmail.com>
2020-06-08 22:17:03 +0400

    used connect redux

--- src/containers/Home/activities.js
+++ src/containers/Home/activities.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { Activities } from '../../components/Activities';
 
-
-
-export const ActivitiesContainer = () => {
-  const { data, loading, dataError } = useSelector(store => store.activities); 
-  return <Activities data={data} loading={loading} error={dataError} />;
+const mapStateToProps = ({ activities: { data, error, loading } }) => {
+  return {
+    data,
+    loading,
+    error
+  };
 };
+
+const mapDispatchToProps = dispatch => {
+  return {
+    destroyTodo: () =>
+      dispatch({
+        type: 'DESTROY_TODO'
+      })
+  }
+}
+const ActivitiesContainer = ({ data, loading, error }) => {
+  return <Activities data={data} loading={loading} error={error} />;
+};
+
+export default   connect(mapStateToProps)(ActivitiesContainer);
