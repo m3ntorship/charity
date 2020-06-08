5db08b44694a2896ebaf85e59e4cb999e6f245dc
AbdoMagdy <abdoman46@hotmail.com>
2020-06-08 18:01:54 +0200

    added data sanitization

--- src/store/actions/news&articles.js
+++ src/store/actions/news&articles.js
@@ -1,22 +1,35 @@
 import { ACTION_TYPES } from '../actionTypes';
+const dataFilter = data => {
+  let filtered = ({
+    heading: { heading_primary, heading_secondary },
+    link: { text, url },
+    home_articles
+  }) =>
+    ({
+      heading: { heading_primary, heading_secondary },
+      link: { text, url },
+      home_articles
+    }(data));
+  return filtered;
+};
 
 export const setNewsAndArticlesData = data => {
   return {
     type: ACTION_TYPES.NEWS_ARTICLES.SET_DATA,
-    payload: data
+    payload: dataFilter(data)
   };
-}
+};
 
 export const setNewsAndArticlesLoading = loading => {
-         return {
-           type: ACTION_TYPES.NEWS_ARTICLES.SET_LOADING,
-           payload: loading
-         };
-       };
+  return {
+    type: ACTION_TYPES.NEWS_ARTICLES.SET_LOADING,
+    payload: loading
+  };
+};
 
 export const setNewsAndArticlesError = errorState => {
-         return {
-           type: ACTION_TYPES.NEWS_ARTICLES.SET_ERROR,
-           payload: errorState
-         };
-       };
+  return {
+    type: ACTION_TYPES.NEWS_ARTICLES.SET_ERROR,
+    payload: errorState
+  };
+};
