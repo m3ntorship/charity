c3754f7c3bd2ce411d39d9090db375c43c343696
malngaawy <mohager477@gmail.com>
2020-06-10 14:40:41 +0200

    handle some responsive styles

--- src/components/HeaderNavigation/index.jsx
+++ src/components/HeaderNavigation/index.jsx
@@ -24,43 +24,45 @@ const HeaderNavigation = ({
     return 'loading';
   }
   return (
-    <section className="header-nav p-0 border-2">
-      <div className="container sm:flex">
-        <div className="flex items-center px-4 py-4 justify-between">
-          <div className="w-24">
-            <Logo data={logoData} />
+    <section className="header-nav py-5 px-0 border-2">
+      <div className="container sm:flex sm:justify-between">
+        <div className="logo-links-container sm:flex sm:justify-between sm:w-full">
+          <div className="flex items-center px-4 py-4 justify-between">
+            <div className="w-24">
+              <Logo data={logoData} />
+            </div>
+            <div className="toggle-btn sm:hidden">
+              <button
+                type="button"
+                className="text-gray-600 bg-c300 block"
+                onClick={() => {
+                  setOpen(!isOpen);
+                }}
+              >
+                {isOpen ? 'Close' : 'Open'}
+              </button>
+            </div>
           </div>
-          <div className="toggle-btn sm:hidden">
-            <button
-              type="button"
-              className="text-gray-600 bg-c300 block"
-              onClick={() => {
-                setOpen(!isOpen);
-              }}
-            >
-              {isOpen ? 'Close' : 'Open'}
-            </button>
-          </div>
+          <ul
+            className={cn(
+              'block sm:flex sm:justify-between sm:items-center sm:ml-10',
+              {
+                hidden: !isOpen
+              }
+            )}
+          >
+            <Router>
+              {pagesData.map(page => (
+                <NavigationLink
+                  url={page.link.url}
+                  text={page.link.text}
+                  className="sm:mx-4 sm:font-bold nav-link"
+                />
+              ))}
+            </Router>
+          </ul>
         </div>
-        <ul
-          className={cn(
-            'block sm:flex sm:justify-between sm:items-center sm:ml-10',
-            {
-              hidden: !isOpen
-            }
-          )}
-        >
-          <Router>
-            {pagesData.map(page => (
-              <NavigationLink
-                url={page.link.url}
-                text={page.link.text}
-                className="sm:mx-4 sm:font-bold"
-              />
-            ))}
-          </Router>
-        </ul>
-        <div className="contacts text-sm mx-6">
+        <div className="hidden contacts text-sm mx-6">
           {contactData
             .filter(
               contact =>
@@ -82,7 +84,7 @@ const HeaderNavigation = ({
                     </div>
                     <div className="information">
                       <a
-                        className="block"
+                        className="block hover:text-c100"
                         href={url}
                         target="_blank"
                         rel="noopener noreferrer"
