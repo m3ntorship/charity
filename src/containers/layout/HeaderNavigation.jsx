8f03f50ec9699fef1d5b6e16a884668bfdb032eb
malngaawy <mohager477@gmail.com>
2020-06-10 18:45:23 +0200

    create header navigation container

---
+++ src/containers/layout/HeaderNavigation.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import HeaderNavigation from '../../components/HeaderNavigation';
+
+const HeaderNavigationContainer = () => {
+  const {
+    data: pagesData,
+    loading: pagesLoading,
+    dataError: pagesError
+  } = useSelector(({ pages }) => pages);
+  const {
+    data: contactData,
+    loading: contactLoading,
+    error: contactError
+  } = useSelector(store => store.contact);
+
+  console.log(pagesData);
+  console.log(contactData);
+
+  return (
+    <HeaderNavigation
+      pagesData={pagesData}
+      pagesLoading={pagesLoading}
+      pagesError={pagesError}
+      contactData={contactData}
+      contactLoading={contactLoading}
+      contactError={contactError}
+    />
+  );
+};
+
+export default HeaderNavigationContainer;
