4dfcfb0a67873e547025e6408605e2f5fa4040c1
malngaawy <mohager477@gmail.com>
2020-06-11 01:42:30 +0200

    delete unused components from header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 // import { MainContactContainer } from '../MainContact';
 import { ContactTopContainer } from '../ContactTop';
-// import { LogoContainer } from '../../containers/layout/logo';
-// import { MainNavigationContainer } from '../MainNavigation';
 import HeaderNavigationContainer from '../../containers/layout/HeaderNavigation';
 const Header = () => (
   <header>
     <ContactTopContainer haveBtn={true} />
-    {/* <section className="logo-section py-8 hidden md:block">
-      <div className="container py-2 flex items-center justify-between">
-        <LogoContainer />
-        <MainContactContainer />
-      </div>
-    </section>
-    <div className="container mainnav__container ">
-      <MainNavigationContainer />
-    </div> */}
     <HeaderNavigationContainer />
   </header>
 );
