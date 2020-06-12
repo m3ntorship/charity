29d9cd9ad3433ff7d813f3c632e19a16451e4074
malngaawy <mohager477@gmail.com>
2020-06-12 23:21:37 +0200

    remove commented imports

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { MainContactContainer } from '../MainContact';
 import { ContactTopContainer } from '../ContactTop';
 import HeaderNavigationContainer from '../../containers/layout/HeaderNavigation';
 const Header = () => (
