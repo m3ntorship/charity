b44f31ad792de25a5931f56e1076d3f77971528d
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-12 15:17:50 +0200

    sanitize data for featured-banner component

--- src/store/actions/featuredBanner.js
+++ src/store/actions/featuredBanner.js
@@ -5,10 +5,35 @@ import { ACTION_TYPES } from '../actionTypes';
 //  setFeaturedBannerLoadingActionCreator,
 // setFeaturedBannerErrorActionCreator
 
+// Sanitizing data
+const sanitizeBanner = (data) => {
+  if(data){
+    const {
+      text_primary,
+      text_complementary,
+      button_text,
+      button_url,
+      image_background: { url: image_background_url },
+      image_top: { url: image_url }
+    } = data;
+    return {
+      text_primary,
+      text_complementary,
+      button_text,
+      button_url,
+      image_url,
+      image_background_url
+    };
+  }
+  else{
+    return data
+  }
+}
+
 export const setFeaturedBannerData = data => {
   return {
     type: ACTION_TYPES.FEATURED_BANNER.SET_DATA,
-    payload: data
+    payload: sanitizeBanner(data)
   };
 };
 
