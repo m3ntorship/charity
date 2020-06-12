4299e915b7e56fe9fa85a4433105aefd2d994336
malngaawy <mohager477@gmail.com>
2020-06-12 12:37:07 +0200

    remove router from link __ it cause error

--- src/components/NavigationLink/index.jsx
+++ src/components/NavigationLink/index.jsx
@@ -3,13 +3,11 @@ import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 const NavigationLink = ({ url, text, className }) => {
   return (
-    <Router>
-      <li className={className}>
-        <Link to={url} className="text-c600 hover:text-c500">
-          {text}
-        </Link>
-      </li>
-    </Router>
+    <li className={className}>
+      <Link to={url} className="text-c600 hover:text-c500">
+        {text}
+      </Link>
+    </li>
   );
 };
 
