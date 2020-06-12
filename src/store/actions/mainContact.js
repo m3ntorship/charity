7811772c5a824fb4317714b46eb92ca2eb647919
malngaawy <mohager477@gmail.com>
2020-06-12 23:26:53 +0200

    applay contact info sanitizing data to main contact reducer

--- src/store/actions/mainContact.js
+++ src/store/actions/mainContact.js
@@ -1,12 +1,26 @@
 import { ACTION_TYPES } from '../actionTypes';
 
+const dataSanitization = data => {
+  if (data) {
+    return data.map(contact => {
+      return {
+        _id: contact._id,
+        title: contact.title,
+        url: contact.url,
+        sub_title: contact.sub_title,
+        icon: { url: contact.icon.url, name: contact.icon.name }
+      };
+    });
+  }
+};
+
 export const setMainContactData = data => {
   return {
     type: ACTION_TYPES.MAIN_CONTACT.SET_DATA,
-    payload: data
+    payload: dataSanitization(data)
   };
 };
-  
+
 export const setMainContactLoading = loading => {
   return {
     type: ACTION_TYPES.MAIN_CONTACT.SET_LOADING,
