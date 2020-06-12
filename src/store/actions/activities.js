ec6b38dfa77d933bb58c33c9c94cb37c67793f25
Zahraa-Khalil <zahraa.khalil2020@gmail.com>
2020-06-12 20:48:24 +0200

    sanitize activities component (#445)
    * sanitize activities component
    
    * sanitize data for featured-banner component
    
    * delete featuredbanner sanitization- wrongBranch
    
    * filter image_main data
    
    * update activity_component data

--- src/store/actions/activities.js
+++ src/store/actions/activities.js
@@ -1,9 +1,42 @@
 import { ACTION_TYPES } from '../actionTypes';
 
+
+const dataSanitize = (data) => {
+  if(data){
+    const { 
+  title_primary,
+  title_complementary,
+  description,
+  how_we_work_cards} = data;
+  const activitiesFilter = how_we_work_cards.map(
+    ({ _id, Title, description, image_main: { url: main_image_url , alternativeText } }) => {
+      return {
+        _id,
+        Title,
+        description,
+        main_image_url,
+        alternativeText
+      };
+    }
+  );
+  return {
+    title_primary,
+    title_complementary,
+    description,
+    how_we_work_cards: activitiesFilter
+  };
+  }
+  else{
+    return data
+  }
+}
+
+
+
 export const setActivitiesData = data => {
   return {
     type: ACTION_TYPES.ACTIVITIES.SET_DATA,
-    payload: data
+    payload: dataSanitize(data)
   };
 };
 
