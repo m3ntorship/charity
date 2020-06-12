c75659c9825c08176297f42478163238f40aa421
Tramsi <tramsi2014@gmail.com>
2020-06-12 21:50:06 +0200

    Sanitizing Articles Data

--- src/store/actions/Articles.js
+++ src/store/actions/Articles.js
@@ -1,9 +1,45 @@
 import { ACTION_TYPES } from '../actionTypes';
-
+const dataSanitization = data => {
+  if (data) {
+    const sanitizedArticles = data.map(
+      ({
+        title,
+        link: { text, url: linkURL },
+        thumbnail: { url: imageURL, alternativeText },
+        id,
+        image_main,
+        body,
+        author: { username },
+        description,
+        createdAt
+      }) => {
+        const image_mainSanitized = image_main.map(({ url }) => {
+          return { url };
+        });
+        return {
+          title,
+          text,
+          linkURL,
+          imageURL,
+          id,
+          image_main: image_mainSanitized,
+          body,
+          username,
+          alternativeText,
+          description,
+          createdAt
+        };
+      }
+    );
+    return sanitizedArticles;
+  } else {
+    return data;
+  }
+};
 export const setArticlesData = data => {
   return {
     type: ACTION_TYPES.ARTICLES.SET_DATA,
-    payload: data
+    payload: dataSanitization(data)
   };
 };
 
