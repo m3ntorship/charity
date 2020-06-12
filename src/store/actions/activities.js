025498824dcb335107d328ff81cab26441a32b6c
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-12 13:16:10 +0200

    sanitize activities component

--- src/store/actions/activities.js
+++ src/store/actions/activities.js
@@ -1,9 +1,46 @@
 import { ACTION_TYPES } from '../actionTypes';
 
+
+const dataSanitize = (data) => {
+  if(data){
+    const { 
+  title_primary,
+  title_complementary,
+  description,
+  how_we_work_cards} = data;
+  const activitiesFilter = how_we_work_cards.map(
+    ({
+      _id,
+      Title,
+      description,
+      image_main
+    }) => {
+      return {
+        _id,
+        Title,
+        description,
+        image_main
+      };
+    }
+  )
+  return {
+    title_primary,
+    title_complementary,
+    description,
+    how_we_work_cards: activitiesFilter
+  };
+  }
+  else{
+    return data
+  }
+}
+
+
+
 export const setActivitiesData = data => {
   return {
     type: ACTION_TYPES.ACTIVITIES.SET_DATA,
-    payload: data
+    payload: dataSanitize(data)
   };
 }
 
