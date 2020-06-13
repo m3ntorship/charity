e39933a3323b1c665667d749446744053c4e8960
Tramsi <tramsi2014@gmail.com>
2020-06-13 13:58:52 +0200

    Added reducer

---
+++ src/store/reducers/contactTop.js
@@ -0,0 +1,20 @@
+import { ACTION_TYPES } from '../actionTypes';
+
+const initialState = {
+  data: null,
+  loading: false,
+  error: false
+};
+
+export const contactTopReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ACTION_TYPES.CONTACT_TOP.SET_DATA:
+      return { ...state, data: payload };
+    case ACTION_TYPES.CONTACT_TOP.SET_LOADING:
+      return { ...state, loading: payload };
+    case ACTION_TYPES.CONTACT_TOP.SET_ERROR:
+      return { ...state, error: payload };
+    default:
+      return state;
+  }
+};
