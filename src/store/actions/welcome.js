b87142b1b40809e679f62603426a5ff42fe6f9a8
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2020-06-13 13:12:45 +0400

    Feature/sanitizing welcome section (#452)
    * initialize data to null
    
    * add data sanintization logic
    
    * consuming data form welcom-section state

--- src/store/actions/welcome.js
+++ src/store/actions/welcome.js
@@ -1,9 +1,43 @@
 import { ACTION_TYPES } from '../actionTypes';
 
+const dataSanitization = data => {
+  if (data) {
+    const {
+      id,
+      Heading: { heading_primary, heading_secondary },
+      link : {text:linkText, url :linkUrl},
+      description,
+      image :{url:imageUrl},
+      WelcomeActions,
+    } = data;
+    const sanitizedelcomActions = WelcomeActions.map(({title,description,id})=>{
+        return {
+          title,
+          description,
+          id
+        }
+    })
+    return {
+        id,
+        heading_primary,
+        heading_secondary,
+        linkText,
+        linkUrl,
+        description,
+        imageUrl,
+        welcome_cards:sanitizedelcomActions
+    };
+  }
+  else {
+    return data
+  }
+};
+
+
 export const setWelcomeData = data => {
   return {
     type: ACTION_TYPES.WELCOME.SET_DATA,
-    payload: data
+    payload: dataSanitization(data)
   };
 };
 
