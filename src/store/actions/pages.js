6ecdbecf984ec181e949bece77ca99044ca68c4b
zahraa333 <zahraa.khalil2020@gmail.com>
2020-06-16 13:45:47 +0200

    filtering page's data

--- src/store/actions/pages.js
+++ src/store/actions/pages.js
@@ -1,9 +1,45 @@
 import { ACTION_TYPES } from '../actionTypes';
 
+
+const sanitizePages = data => {
+  console.log(data);
+  if (data) {
+    const sanitizeData = data.map(
+      ({
+        show_in_navigation,
+        name,
+        Banner: { title, sub_title, image_bg },
+        name: pageName,
+        link,
+        secondary_banner
+      }) => {
+        return {
+          show_in_navigation,
+          link,
+          name,
+          image_bg,
+          title,
+          sub_title,
+          pageName,
+          secondary_banner
+        };
+      }
+    );
+    return sanitizeData;
+  }
+  else {
+    return data;
+  }
+}
+
+
+
+
+
 export const setPagesDataActionCreator = data => {
   return {
     type: ACTION_TYPES.PAGES.SET_DATA,
-    payload: data
+    payload: sanitizePages(data)
   };
 };
 
