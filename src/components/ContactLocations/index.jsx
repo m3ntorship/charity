908057ec50758b4badd99f22e034b1cbda6177c0
Mohamed Hassan <mohamed.hassn.dev@gmail.com>
2020-06-23 15:38:42 +0200

    make all info about location in a  signle component

--- src/components/ContactLocations/index.jsx
+++ src/components/ContactLocations/index.jsx
@@ -41,55 +41,44 @@ export default function ContactLocations() {
   return <div>Generic Error!</div>;
 }
 
-const Location = ({ city, location, email, phone }) => {
+export const Location = ({ city, location, email, phone }) => {
   return (
     <div className="container__location p-10">
       <h6 className="location-city text-lg pl-2 text-c000">{city}</h6>
+      <InfoWithIcon
+        icon={location.icon.url}
+        infoTitle=""
+        info={location.info}
+      />
+      <InfoWithIcon
+        icon={email.icon.url}
+        infoTitle="Email Address"
+        info={email.info}
+      />
+      <InfoWithIcon
+        icon={phone.icon.url}
+        infoTitle="Phone line"
+        info={phone.info}
+      />
+    </div>
+  );
+};
 
-      <div className="flex pt-8 items-center">
-        <span className="flex-shrink-0">
-          <img
-            className="contact-location-icons object-fill object-cover"
-            src={location.icon.url}
-            alt=""
-          />
-        </span>
+export const InfoWithIcon = ({ icon, infoTitle, info }) => {
+  return (
+    <div className="flex items-center mt-5">
+      <span className="flex-shrink-0">
+        <img
+          className="contact-location-icons object-fill object-cover"
+          src={icon}
+          alt=""
+        />
+      </span>
 
-        <p className="location-address pl-4 text-sm leading-normal text-c000">
-          {location.info}
-        </p>
-      </div>
-
-      <div className="flex items-center mt-5">
-        <span className="flex-shrink-0">
-          <img
-            className="contact-location-icons object-cover object-fill"
-            src={email.icon.url}
-            alt=""
-          />
-        </span>
-
-        <p className="email-address pl-4 text-c000 text-sm">
-          {email.info}
-          <br />
-          <span className="email-label text-xs text-c700">Email Address</span>
-        </p>
-      </div>
-
-      <div className="flex items-center mt-5">
-        <span className="flex-shrink-0">
-          <img
-            className="contact-location-icons object-fill object-cover"
-            src={phone.icon.url}
-            alt=""
-          />
-        </span>
-
-        <p className="phone-number pl-4 text-sm text-c000">
-          {phone.info} <br />
-          <span className="phone-label text-c700 text-xs">Phone line</span>
-        </p>
-      </div>
+      <p className="phone-number pl-4 text-sm text-c000">
+        {info} <br />
+        <span className="phone-label text-c700 text-xs">{infoTitle}</span>
+      </p>
     </div>
   );
 };
