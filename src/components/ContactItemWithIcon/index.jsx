8bd36dda743ad3c2924badc0361881e6bf51c632
Alaa <Alaa.Saedy.eg@gmail.com>
2020-08-22 18:27:26 +0200

    creating contact item component, make OurMission section responsive in different screen sizes and adding svg related img

--- src/components/ContactItemWithIcon/index.jsx
+++ src/components/ContactItemWithIcon/index.jsx
@@ -5,17 +5,17 @@ import './style.css';
 const ContactItemWithIcon = ({ textColor, data }) => {
   const { contactIcon, contactTitle, contactInfo } = data;
   return (
-    <div className=" py-5 lg:flex items-center justify-between sm:grid sm:grid-rows-2">
-      <div className="contactItem__bg w-20 h-20 ">
+    <div className=" py-5 lg:flex items-center lg:justify-between sm:grid sm:grid-rows-2">
+      <div className="contactItem__bg w-20 h-20 mx-auto lg:ml-0">
         <img className=" pt-6 pl-6" src={contactIcon} alt="" />
       </div>
       <div
-        className={cn({
+        className={cn('text-center lg:text-left lg:mt-0 mt-5', {
           'text-c000': !textColor || textColor === 'white',
           'text-c100': textColor === 'black'
         })}
       >
-        <span className="text-sm">{contactTitle}</span>
+        <span className="text-sm inline-block">{contactTitle}</span>
         <div className="text-large">{contactInfo}</div>
       </div>
     </div>
--- src/components/OurMissionSection/index.jsx
+++ src/components/OurMissionSection/index.jsx
@@ -4,24 +4,21 @@ import ContactItemWithIcon from '../ContactItemWithIcon';
 import './style.css';
 
 const OurMission = ({ data }) => {
-  const { mainTitle, subTitle, desc, sectionBg } = data;
+  const { mainTitle, subTitle, desc } = data;
   return (
     <div className="relative bg-c900 section__wrapper">
-      <div className="container grid grid-cols-8 pt-32">
-        <div className="col-span-4">
+      <div className="container grid lg:grid-cols-8 sm:grid-cols-1 pt-32 pb-24">
+        <div className="lg:col-span-4 text-center lg:text-left">
           <Heading
             primaryText={mainTitle}
             secondaryText={subTitle}
             align="left"
           />
           <p className="text-c000">{desc}</p>
-          <div className="w-3/4 my-16">
+          <div className="w-3/4 mt-16 lg:ml-0 mx-auto ">
             <ContactItemWithIcon data={data} />
           </div>
         </div>
-      </div>
-      <div className="section_img">
-        <img className="min-h-full" src={sectionBg} alt="" />
       </div>
     </div>
   );
