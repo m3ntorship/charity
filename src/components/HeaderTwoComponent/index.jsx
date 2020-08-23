928ec98141276bbe9ad8e5de2ba7d2b49422d126
Alaa <Alaa.Saedy.eg@gmail.com>
2020-08-23 03:44:08 +0200

    modified data to be passed dynamically

--- src/components/HeaderTwoComponent/index.jsx
+++ src/components/HeaderTwoComponent/index.jsx
@@ -3,25 +3,31 @@ import Heading from '../Heading';
 import Button from '../ButtonComponent';
 import './style.css';
 
-const HeaderSection = () => {
+const HeaderSection = ({ data }) => {
+  const {
+    headerDesc,
+    title,
+    subTitle,
+    btnTitle,
+    greenBall,
+    yelloBall,
+    redBall,
+    blueBall
+  } = data;
   return (
     <div className="relative bg-c900 grid lg:grid-cols-4 grid-cols-1 lg:p-48">
       <div className="container col-span-3 lg:py-0 py-12 mx-auto lg:ml-0 text-center lg:text-left">
-        <h5 className="text-c300 underline">Welcome to Lovims Theme</h5>
+        <h5 className="text-c300 underline">{subTitle}</h5>
         <Heading
           size="xxl"
-          primaryText="Discover Non-Profit Chairty Platform"
+          primaryText={title}
           primaryClassName="mt-6 lg:text-left text-center"
         />
-        <p className="text-c000 text-md">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime autem
-          quis reprehenderit corrupti ipsum minus voluptatibus aut omnis
-          mollitia? Doloribus cupiditate excepturi.
-        </p>
+        <p className="text-c000 text-md">{headerDesc}</p>
         <div className="mt-8 lg:mb-0 mb-8">
           <Button
             btnSize="large"
-            btnText="View Causes"
+            btnText={btnTitle}
             btnTextColor="white"
             bgColor="red"
           />
@@ -29,22 +35,22 @@ const HeaderSection = () => {
       </div>
       <img
         className="hidden lg:block green__ball w-40 absolute"
-        src="https://i.imgur.com/04LyZnm.png"
+        src={greenBall}
         alt=""
       />
       <img
         className="hidden lg:block yellow__ball h-32 absolute"
-        src="https://i.imgur.com/WifmwSJ.png"
+        src={yelloBall}
         alt=""
       />
       <img
         className="hidden lg:block red__ball h-32 absolute"
-        src="https://i.imgur.com/hrWuYE5.png"
+        src={redBall}
         alt=""
       />
       <img
         className="hidden lg:block blue__ball w-20 absolute"
-        src="https://i.imgur.com/zH0VhYK.png"
+        src={blueBall}
         alt=""
       />
     </div>
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -6,6 +6,18 @@ export default {
   component: HeaderSection
 };
 
+const data = {
+  sunTitle: 'Welcome to Lovims Theme',
+  title: 'Discover Non-Profit Chairty Platform',
+  headerDesc:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime autem quis reprehenderit corrupti ipsum minus voluptatibus aut omnis mollitia? Doloribus cupiditate excepturi.',
+  btnTitle: 'View Causes',
+  greenBall: 'https://i.imgur.com/04LyZnm.png',
+  yelloBall: 'https://i.imgur.com/WifmwSJ.png',
+  redBall: 'https://i.imgur.com/hrWuYE5.png',
+  blueBall: 'https://i.imgur.com/zH0VhYK.png'
+};
+
 export const HeaderShowCase = () => {
-  return <HeaderSection />;
+  return <HeaderSection data={data} />;
 };
