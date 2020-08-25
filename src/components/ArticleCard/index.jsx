f52a48e71fe4df6f16f08578f54b3b31ad22fd64
Alaa <Alaa.Saedy.eg@gmail.com>
2020-08-25 18:30:02 +0200

    restructure the article card

--- src/components/ArticleCard/index.jsx
+++ src/components/ArticleCard/index.jsx
@@ -3,14 +3,16 @@ import './style.css';
 
 const ArticleCard = () => {
   return (
-    <div className="relative card__box bg-c100 border my-24">
-      <img
-        className="h-full object-cover"
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt=""
-      />
+re    <div className="relative grid grid-rows-3 max-w-md card__box my-12">
+      <div className="row-start-1 row-end-4 col-start-1 bg-c100">
+        <img
+          className="h-full object-cover"
+          src="https://tailwindcss.com/img/card-top.jpg"
+          alt=""
+        />
+      </div>
 
-      <div className="articleTitle__box p-10 bg-c000 text-c700">
+      <div className="absolute articleTitle__box p-10 bg-c000 row-start-3 col-start-1 z-50 text-c700">
         <div className="flex justify-around">
           <span>
             <i className="fa fa-user-circle mr-2 text-c500"></i>Admin
