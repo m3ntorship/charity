9cfd88427e94ea151873a25042e7168d684d5a45
Alaa <Alaa.Saedy.eg@gmail.com>
2020-08-25 17:55:11 +0200

    Creating Article Card Component

---
+++ src/stories/ArticleCard.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ArticleCard from '../components/ArticleCard';
+
+export default {
+  title: 'ArticleCard',
+  component: ArticleCard
+};
+
+export const ArticleCardItem = () => {
+  return <ArticleCard />;
+};
